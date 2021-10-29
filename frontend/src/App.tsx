cde99a07dcc39755d0639d807ed6c9c69b9fe34c
takamuu <mr.takamuu@gmail.com>
2021-10-29 11:02:16 +0900

    Initial setting of react completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
 
-function App() {
-  return (
-    <>
-      <p>Hello React!</p>
-    </>
-  );
+export default function App() {
+  return <h1>Hello World!</h1>;
 }
-
-export default App;
