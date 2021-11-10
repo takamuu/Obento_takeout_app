591845a44a22bc9d93725ba733b51a2ff982d2b6
takamuu <mr.takamuu@gmail.com>
2021-11-10 11:05:29 +0900

    Added Atomic design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import { Button, ChakraProvider } from '@chakra-ui/react';
-import './App.css';
+import axios from 'axios';
+
+import theme from './theme/theme';
+import { Todo } from './Todo';
+
+type TodoType = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 export default function App(): JSX.Element {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>('https://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
-    <ChakraProvider>
-      <Button colorScheme="teal">ボタン</Button>
+    <ChakraProvider theme={theme}>
+      <Button colorScheme="teal" onClick={onClickFetchData}>
+        ボタン
+      </Button>
+      {todos.map((todo, i: number) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </ChakraProvider>
   );
 }
