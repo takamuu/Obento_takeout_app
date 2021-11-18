54260733e7b3917fb0ac2876abb3bb75a5a53b3d
takamuu <mr.takamuu@gmail.com>
2021-11-18 17:22:24 +0900

    Processing and error alerts after data acquisition #29

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -1,11 +1,26 @@
+/* eslint-disable arrow-body-style */
 import axios from 'axios';
 import { useCallback } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import { User } from 'types/api/user';
 
 export const useAuth = () => {
-  const login = useCallback((id: string) => {
-    axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`);
-  }, []);
+  const history = useHistory();
+  const login = useCallback(
+    (id: string) => {
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            history.push('/restaurants');
+          } else {
+            alert('ユーザーが見つかりません');
+          }
+        })
+        .catch(() => alert('ログインできません'));
+    },
+    [history]
+  );
   return { login };
 };
