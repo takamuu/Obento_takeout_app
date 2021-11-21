94b1f564b23faf244359d01c60f9ba4fdfe8e83a
takamuu <mr.takamuu@gmail.com>
2021-11-21 12:09:37 +0900

    Create the restaurant type #34

---
+++ frontend/src/types/api/restaurant.ts
@@ -0,0 +1,16 @@
+export type Restaurant = {
+  id: number;
+  name: string;
+  description: string;
+  fee: number;
+  postal_code: number;
+  prefecture_code: string;
+  prefecture: string;
+  city: string;
+  block_building: string;
+  phone_number: string;
+  update_time: Date;
+  image: string;
+  created_at: Date;
+  updated_at: Date;
+};
