ae3cf768bd0e474ced17d5ca8b865cd331f12119
takamuu <mr.takamuu@gmail.com>
2021-11-21 12:12:49 +0900

    Create a restaurant list page #34

--- frontend/src/components/pages/Restaurants.tsx
+++ frontend/src/components/pages/Restaurants.tsx
@@ -1,21 +1,36 @@
-import { memo, useEffect, VFC } from 'react';
-import { Link } from 'react-router-dom';
+import { memo, VFC } from 'react';
+import { Wrap, WrapItem } from '@chakra-ui/layout';
 
-import { fetchRestaurants } from 'apis/restaurants';
-
+import HappyHour from 'images/HappyHour.jpg';
+import { RestaurantCard } from 'components/organisms/restaurant/RestaurantCard';
 export const Restaurants: VFC = memo(() => {
-  useEffect(() => {
-    fetchRestaurants().then((data) => {
-      console.log(data);
-    });
-  }, []);
   return (
-    <div>
-      <p>HOMEページ = レストラン一覧ページ</p>
-      <br />
-      <Link to="/restaurants/100/foods">Foods</Link>
-      <br />
-      <Link to="/restaurants/100/foods?name=hogehoge">Query Parameter</Link>
-    </div>
+    <Wrap p={{ base: 4, md: 10 }}>
+      <WrapItem>
+        <RestaurantCard imageUrl={HappyHour} restaurantName="HappyHour" />
+      </WrapItem>
+    </Wrap>
   );
 });
+
+// import { memo, useEffect, VFC } from 'react';
+// import { Link } from 'react-router-dom';
+
+// import { fetchRestaurants } from 'apis/restaurants';
+
+// export const Restaurants: VFC = memo(() => {
+//   useEffect(() => {
+//     fetchRestaurants().then((data) => {
+//       console.log(data);
+//     });
+//   }, []);
+//   return (
+//     <div>
+//       <p>HOMEページ = レストラン一覧ページ</p>
+//       <br />
+//       <Link to="/restaurants/100/foods">Foods</Link>
+//       <br />
+//       <Link to="/restaurants/100/foods?name=hogehoge">Query Parameter</Link>
+//     </div>
+//   );
+// });
