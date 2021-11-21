3df1df481f145c318ec4392d9c75d91bbb16cba3
takamuu <mr.takamuu@gmail.com>
2021-11-21 15:08:51 +0900

    Fix eslint support #34

--- frontend/src/url/index.tsx
+++ frontend/src/url/index.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable arrow-body-style */
 const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
 
 export const restaurantsIndexUrl = `${DEFAULT_API_LOCALHOST}/restaurants`;
-export const foodsIndex = (restaurantId: any) =>
+export const foodsIndex = (restaurantId: number) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
 // export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods`;
 // export const lineFoodsReplace = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
