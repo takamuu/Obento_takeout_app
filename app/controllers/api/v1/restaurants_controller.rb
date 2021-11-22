17793865aa798dc43a178179e285b3f980f25aa6
takamuu <mr.takamuu@gmail.com>
2021-11-23 01:18:16 +0900

    Set ascending order for controllers #34

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RestaurantsController < ApplicationController
   def index
-    restaurants = Restaurant.all
+    restaurants = Restaurant.all.order(id: "ASC")
 
     render json: restaurants, status: :ok
   end
