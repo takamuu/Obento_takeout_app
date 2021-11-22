521d580f623fdca21bb28e041c55194db2f76105
takamuu <mr.takamuu@gmail.com>
2021-11-22 15:34:23 +0900

    Create useRestaurants custom hook #34

--- frontend/src/hooks/useRestaurants.ts
+++ frontend/src/hooks/useRestaurants.ts
@@ -10,22 +10,21 @@ import { useMessage } from './useMessage';
 export const useRestaurants = () => {
   const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
-  const [restaurants, setRestaurants] = useState<Array<Restaurant>>();
+  const [restaurants, setRestaurants] = useState<Array<Restaurant>>([]);
 
   const getRestaurants = useCallback(() => {
     setLoading(true);
     axios
       .get<Array<Restaurant>>(restaurantsIndexUrl)
-      .then((res) => {
-        setRestaurants(res.data);
-        console.log(res.data);
-      })
-      .catch((e) => {
-        showMessage({ title: 'ユーザー取得に失敗しました', status: 'error' });
-        console.error(e);
+      .then((res) => setRestaurants(res.data))
+      .catch(() => {
+        showMessage({
+          title: 'レストランデータの取得に失敗しました',
+          status: 'error',
+        });
       })
       .finally(() => setLoading(false));
   }, []);
 
-  return { getRestaurants };
+  return { getRestaurants, loading, restaurants };
 };
