9ab016f0b4378dc712d38c4977540e4397a90a3d
takamuu <mr.takamuu@gmail.com>
2021-11-24 20:08:15 +0900

    Created custom hooks(useFoods.ts) #36

---
+++ frontend/src/hooks/useFoods.ts
@@ -0,0 +1,33 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable arrow-body-style */
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+
+import { Food } from 'types/api/food';
+import { foodsIndexUrl } from 'url/index';
+import { useMessage } from './useMessage';
+
+export const useFoods = () => {
+  const { showMessage } = useMessage();
+  const [loading, setLoading] = useState(false);
+  const [foods, setFoods] = useState<Array<Food>>([]);
+
+  const getFoods = useCallback((restaurantId) => {
+    axios
+      .get<Array<Food>>(foodsIndexUrl(restaurantId))
+      .then((res) => {
+        setFoods(res.data);
+      })
+      .catch(() => {
+        showMessage({
+          title: 'フードデータの取得に失敗しました',
+          status: 'error',
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return { getFoods, loading, foods };
+};
