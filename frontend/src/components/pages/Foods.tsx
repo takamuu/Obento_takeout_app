4e9a08b955287a743b8f3ef28a5700127ce96833
takamuu <mr.takamuu@gmail.com>
2021-11-24 20:09:06 +0900

    Created Foods page #36

--- frontend/src/components/pages/Foods.tsx
+++ frontend/src/components/pages/Foods.tsx
@@ -1,7 +1,53 @@
+/* eslint-disable no-constant-condition */
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable arrow-body-style */
+import { memo, useEffect, VFC } from 'react';
+import { Center, Wrap, WrapItem } from '@chakra-ui/layout';
+import { Spinner } from '@chakra-ui/spinner';
+
+import { useFoods } from 'hooks/useFoods';
+import { FoodCard } from 'components/organisms/food/FoodCard';
+import 牛タン弁当 from 'images/牛タン弁当.svg';
+import { useParams } from 'react-router-dom';
+
+type IdType = {
+  restaurantId: string;
+};
+
+export const Foods: VFC = memo(() => {
+  const { getFoods, foods, loading } = useFoods();
+  const { restaurantId } = useParams<IdType>();
+
+  useEffect(() => getFoods(restaurantId), []);
+
+  return (
+    <>
+      {loading ? (
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      ) : (
+        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+          {foods.map((food) => (
+            <WrapItem key={food.id}>
+              <FoodCard
+                imageUrl={牛タン弁当}
+                foodName={food.name}
+                foodDescription={food.food_description}
+                foodPrice={food.price}
+              />
+            </WrapItem>
+          ))}
+        </Wrap>
+      )}
+    </>
+  );
+});
+
 // Foodsページ実装時にコメントアウト解除予定（エラーが出るので一時的にコメントアウト）
 
 // import { fetchRestaurants } from 'apis/restaurants';
-import { memo, useEffect, VFC } from 'react';
+// import { memo, useEffect, VFC } from 'react';
 // import { useParams, useLocation } from 'react-router';
 
 // type IdType = {
