538c8c2edc725f66c6ca69df10aa4f3363d1e05a
takamuu <mr.takamuu@gmail.com>
2021-11-24 17:56:18 +0900

    Change foodsIndex to foodsIndexUrl #36

--- frontend/src/url/index.tsx
+++ frontend/src/url/index.tsx
@@ -2,7 +2,7 @@
 const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
 
 export const restaurantsIndexUrl = `${DEFAULT_API_LOCALHOST}/restaurants`;
-export const foodsIndex = (restaurantId: number) =>
+export const foodsIndexUrl = (restaurantId: number) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
 // export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods`;
 // export const lineFoodsReplace = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
