0f191b60e090fe57bfc6d0ee0ca95b6bd1f0008e
takamuu <mr.takamuu@gmail.com>
2021-11-26 04:47:30 +0900

    Added div tag for confirmation #36

--- frontend/src/components/pages/Foods.tsx
+++ frontend/src/components/pages/Foods.tsx
@@ -2,7 +2,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable arrow-body-style */
 import { memo, useEffect, VFC } from 'react';
-import { Center, Wrap, WrapItem } from '@chakra-ui/layout';
+import { Box, Center, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
 import { useParams } from 'react-router-dom';
 
@@ -28,18 +28,22 @@ export const Foods: VFC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap p={{ base: 4, md: 10 }} justify="space-around">
-          {foods.map((food) => (
-            <WrapItem key={food.id}>
-              <FoodCard
-                imageUrl={牛タン弁当}
-                foodName={food.name}
-                foodDescription={food.food_description}
-                foodPrice={food.price}
-              />
-            </WrapItem>
-          ))}
-        </Wrap>
+        // 挙動確認用のdivタグを追加（Foodモーダル実装時にdivタグとレストランIDは削除）
+        <div>
+          レストラン：{restaurantId}
+          <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+            {foods.map((food) => (
+              <WrapItem key={food.id}>
+                <FoodCard
+                  imageUrl={牛タン弁当}
+                  foodName={food.name}
+                  foodDescription={food.food_description}
+                  foodPrice={food.price}
+                />
+              </WrapItem>
+            ))}
+          </Wrap>
+        </div>
       )}
     </>
   );
