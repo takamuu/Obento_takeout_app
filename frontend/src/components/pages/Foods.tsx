f442616d326a6397ff0cb49a92be3a01e46d3900
takamuu <mr.takamuu@gmail.com>
2021-11-26 04:22:15 +0900

    Added useParams to Foods components #36

--- frontend/src/components/pages/Foods.tsx
+++ frontend/src/components/pages/Foods.tsx
@@ -4,11 +4,11 @@
 import { memo, useEffect, VFC } from 'react';
 import { Center, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
+import { useParams } from 'react-router-dom';
 
 import { useFoods } from 'hooks/useFoods';
 import { FoodCard } from 'components/organisms/food/FoodCard';
 import 牛タン弁当 from 'images/牛タン弁当.svg';
-import { useParams } from 'react-router-dom';
 
 type IdType = {
   restaurantId: string;
@@ -16,6 +16,7 @@ type IdType = {
 
 export const Foods: VFC = memo(() => {
   const { getFoods, foods, loading } = useFoods();
+
   const { restaurantId } = useParams<IdType>();
 
   useEffect(() => getFoods(restaurantId), []);
@@ -43,33 +44,3 @@ export const Foods: VFC = memo(() => {
     </>
   );
 });
-
-// Foodsページ実装時にコメントアウト解除予定（エラーが出るので一時的にコメントアウト）
-
-// import { fetchRestaurants } from 'apis/restaurants';
-// import { memo, useEffect, VFC } from 'react';
-// import { useParams, useLocation } from 'react-router';
-
-// type IdType = {
-//   restaurantId: string;
-// };
-
-// export const Foods: VFC = memo(() => {
-//   useEffect(() => {
-//     fetchRestaurants().then((data) => {
-//       console.log(data);
-//     });
-//   }, []);
-//   const { restaurantId } = useParams<IdType>();
-//   const { search } = useLocation();
-//   const query = new URLSearchParams(search);
-//   console.log(query);
-//   return (
-//     <div>
-//       <h1>Foodsページ</h1>
-//       <p>restaurantIdは {restaurantId}</p>
-//       <p>クエリパラメーターは{query.get('name')}です</p>
-//       {/* <p>{data}</p> */}
-//     </div>
-//   );
-// });
