37f49d6ab4993ec3e34ae851bd8617784704ea20
takamuu <mr.takamuu@gmail.com>
2021-11-29 07:34:20 +0900

    Implemented useDisclosure and onClickFood on the Food page #37

--- frontend/src/components/pages/Foods.tsx
+++ frontend/src/components/pages/Foods.tsx
@@ -1,21 +1,30 @@
 /* eslint-disable no-constant-condition */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable arrow-body-style */
-import { memo, useEffect, VFC } from 'react';
+import { memo, useCallback, useEffect, VFC } from 'react';
 import { Center, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
 import { useParams } from 'react-router-dom';
+import { useDisclosure } from '@chakra-ui/hooks';
 
 import { useFoods } from 'hooks/useFoods';
 import { FoodCard } from 'components/organisms/food/FoodCard';
 import BeefTongue from 'images/BeefTongue.svg';
+import { FoodDetailModal } from 'components/organisms/food/FoodDetailModal';
 
 export const Foods: VFC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const { getFoods, foods, loading } = useFoods();
 
   const { restaurantId } = useParams<{ restaurantId: string }>();
 
   useEffect(() => getFoods(restaurantId), []);
+
+  const onClickFood = useCallback((id: number) => {
+    console.log(id);
+    onOpen();
+  }, []);
 
   return (
     <>
@@ -31,16 +40,19 @@ export const Foods: VFC = memo(() => {
             {foods.map((food) => (
               <WrapItem key={food.id}>
                 <FoodCard
+                  id={food.id}
                   imageUrl={BeefTongue}
                   foodName={food.name}
                   foodDescription={food.food_description}
                   foodPrice={food.price}
+                  onClick={onClickFood}
                 />
               </WrapItem>
             ))}
           </Wrap>
         </div>
       )}
+      <FoodDetailModal isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
