db2de2b9e95ea0e984a5f3fb2290873db0f6fcce
takamuu <mr.takamuu@gmail.com>
2021-11-29 23:23:31 +0900

    Add modal display function to custom hook #37

--- frontend/src/hooks/useSelectFood.ts
+++ frontend/src/hooks/useSelectFood.ts
@@ -6,15 +6,18 @@ import { Food } from 'types/api/food';
 type Props = {
   id: number;
   foods: Array<Food>;
+  onOpen: () => void;
 };
 
+// 選択したフード情報を特定しモーダルを表示するカスタムフック
 export const useSelectFood = () => {
   const [selectedFood, setSelectedFood] = useState<Food | null>(null);
 
   const onSelectFood = useCallback((props: Props) => {
-    const { id, foods } = props;
+    const { id, foods, onOpen } = props;
     const targetFood = foods.find((food) => food.id === id);
     setSelectedFood(targetFood);
+    onOpen();
   }, []);
 
   return { onSelectFood, selectedFood };
