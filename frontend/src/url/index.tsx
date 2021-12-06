26d7cb5a5bc9cdb8491c42636ee139ca187ed685
takamuu <mr.takamuu@gmail.com>
2021-12-06 19:15:21 +0900

    Added index url #43

--- frontend/src/url/index.tsx
+++ frontend/src/url/index.tsx
@@ -4,6 +4,6 @@ const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
 export const restaurantsIndexUrl = `${DEFAULT_API_LOCALHOST}/restaurants`;
 export const foodsIndexUrl = (restaurantId: string) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
-// export const lineFoods = `${DEFAULT_API_LOCALHOST}/line_foods`;
-// export const lineFoodsReplace = `${DEFAULT_API_LOCALHOST}/line_foods/replace`;
-// export const orders = `${DEFAULT_API_LOCALHOST}/orders`;
+export const cart = `${DEFAULT_API_LOCALHOST}/cart`;
+export const cartReplace = `${DEFAULT_API_LOCALHOST}/cart/replace`;
+export const orders = `${DEFAULT_API_LOCALHOST}/orders`;
