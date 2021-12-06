bd46affce07a5c0c03113fd489d9f9b65690afa0
takamuu <mr.takamuu@gmail.com>
2021-12-06 17:12:06 +0900

    Create index method #44

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -1,44 +1,61 @@
 class Api::V1::CartsController < ApplicationController
-  # before_action :set_food, only: %i[create]
+  before_action :set_food, only: %i[create]
 
-  #   def create
-  #     if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
-  #       return render json: {
-  #         existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
-  #         new_restaurant: Food.find(params[:food_id]).restaurant.name,
-  #       }, status: :not_acceptable
-  #     end
+  def index
+    carts = Cart.find_by(user_id:1)
+    #  binding.pry
+    if carts.present?
+      render json: [
+        cart_ids: carts.id,
+        restaurant: carts.cart_details_foods.first.restaurant,
+        foods: carts.cart_details_foods,
+        total_price: carts.total_price,
+      ], status: :ok
+    else
+      # binding.pry
+      render json: [], status: :no_content
+    end
+  end
 
-  #     set_line_food(@ordered_food)
+  def create
+    all_food_in_cart = User.first.cart.foods
+    if @ordered_food.restaurant_id != all_food_in_cart.first
+      return render json: {
+        existing_restaurant: all_food_in_cart.first.restaurant.name,
+        new_restaurant: Food.find(params[:food_id]).restaurant.name,
+      }, status: :not_acceptable
+    end
 
-  #     if @line_food.save
-  #       render json: {
-  #         line_food: @line_food
-  #       }, status: :created
-  #     else
-  #       render json: {}, status: :internal_server_error
-  #     end
-  #   end
+    set_cart(@ordered_food)
 
-  #   private
+    if @cart.save
+      render json: {
+        cart: @cart
+      }, status: :created
+    else
+      render json: {}, status: :internal_server_error
+    end
+  end
 
-  #   def set_food
-  #     @ordered_food = Food.find(params[:food_id])
-  #   end
+    private
 
-  #   def set_line_food(ordered_food)
-  #     if ordered_food.line_food.present?
-  #       @line_food = ordered_food.line_food
-  #       @line_food.attributes = {
-  #         count: ordered_food.line_food.count + params[:count],
-  #         active: true
-  #       }
-  #     else
-  #       @line_food = ordered_food.build_line_food(
-  #         count: params[:count],
-  #         restaurant: ordered_food.restaurant,
-  #         active: true
-  #       )
-  #     end
-  #   end
+    def set_food
+      @ordered_food = Food.find(params[:food_id])
+    end
+
+    def set_cart(ordered_food)
+      if ordered_food.cart.present?
+        @cart = ordered_food.cart
+        @cart.attributes = {
+          count: ordered_food.cart.count + params[:count],
+          active: true
+        }
+      else
+        @cart = ordered_food.build_cart(
+          count: params[:count],
+          restaurant: ordered_food.restaurant,
+          active: true
+        )
+      end
+    end
 end
