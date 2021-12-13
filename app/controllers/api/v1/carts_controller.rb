90833a4b521037fa2c90f5ce14dbb3a99ccfc641
takamuu <mr.takamuu@gmail.com>
2021-12-13 13:24:23 +0900

    Fix carts#create method #43

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::CartsController < ApplicationController
 
   def index
     carts = Cart.find_by(user_id:1)
-    #  binding.pry
     if carts.present?
       render json: [
         cart_ids: carts.id,
@@ -12,30 +11,31 @@ class Api::V1::CartsController < ApplicationController
         total_price: carts.total_price,
       ], status: :ok
     else
-      # binding.pry
       render json: [], status: :no_content
     end
   end
 
   def create
     # 暫定的にuserをid:1に限定（ログイン機能実装時に、User判定ロジックを追加）
-    all_food_in_cart = User.find_by(id:1).cart.foods
-    if @ordered_food.restaurant_id != all_food_in_cart.first.restaurant_id
-      return render json: {
-        existing_restaurant: all_food_in_cart.first.restaurant.name,
-        new_restaurant: Food.find(params[:food_id]).restaurant.name,
-      }, status: :not_acceptable
-    end
+    # all_food_in_cart = User.find_by(id:1).cart.foods
+    # if @ordered_food.restaurant_id != all_food_in_cart.first.restaurant_id
+    #   return render json: {
+    #     existing_restaurant: all_food_in_cart.first.restaurant.name,
+    #     new_restaurant: Food.find(params[:food_id]).restaurant.name,
+    #   }, status: :not_acceptable
+    # end
 
+    # パラムスから取得してきたFoodを取得する
     set_cart(@ordered_food)
 
-    if @cart_details.save
-      render json: {
-        cart: @cart_details
-      }, status: :created
-    else
-      render json: {}, status: :internal_server_error
-    end
+    # todo: カートindexを実装時に必要であればrenderを実装する
+    #   if @cart_details.save
+    #     render json: {
+    #       cart: @cart_details
+    #     }, status: :created
+    #   else
+    #     render json: {}, status: :internal_server_error
+    #   end
   end
 
     private
@@ -45,16 +45,28 @@ class Api::V1::CartsController < ApplicationController
     end
 
     def set_cart(ordered_food)
-      if ordered_food.cart_details.present?
-        @cart_details = ordered_food.cart_details.first
-        @cart_details.attributes = {
-          count: ordered_food.cart_details.first.count + params[:count]
-        }
-      else
-        @cart_details = ordered_food.build_cart(
-          count: params[:count],
-          restaurant: ordered_food.restaurant,
-        )
+      # 暫定ログインユーザー
+      test_user = User.first
+      if test_user.cart.blank?
+        # カートを作成
+        Cart.create(user_id:test_user.id, total_price: @ordered_food.price * params[:count])
       end
+      # カート詳細情報を作成
+      test_user.cart.cart_details.create(
+        food_id: params[:food_id],
+        count: params[:count]
+      )
     end
+
+    # todo: 更新メソッド時に以下を使用
+    #   ordered_food.cart_details.first
+    #   @cart_details.attributes = {
+    #     count: ordered_food.cart_details.first.count + params[:count]
+    #   }
+    # else
+    #  ordered_food.new(
+    #   count: params[:count],
+    #   restaurant: ordered_food.restaurant,
+    # )
+    # end
 end
