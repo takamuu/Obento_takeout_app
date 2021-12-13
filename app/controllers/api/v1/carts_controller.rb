574f17b996c018e453ba675b2e0b19f3a699ed51
takamuu <mr.takamuu@gmail.com>
2021-12-13 13:36:28 +0900

    Refactoring carts_controller and cart Models #43

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -16,26 +16,7 @@ class Api::V1::CartsController < ApplicationController
   end
 
   def create
-    # 暫定的にuserをid:1に限定（ログイン機能実装時に、User判定ロジックを追加）
-    # all_food_in_cart = User.find_by(id:1).cart.foods
-    # if @ordered_food.restaurant_id != all_food_in_cart.first.restaurant_id
-    #   return render json: {
-    #     existing_restaurant: all_food_in_cart.first.restaurant.name,
-    #     new_restaurant: Food.find(params[:food_id]).restaurant.name,
-    #   }, status: :not_acceptable
-    # end
-
-    # パラムスから取得してきたFoodを取得する
     set_cart(@ordered_food)
-
-    # todo: カートindexを実装時に必要であればrenderを実装する
-    #   if @cart_details.save
-    #     render json: {
-    #       cart: @cart_details
-    #     }, status: :created
-    #   else
-    #     render json: {}, status: :internal_server_error
-    #   end
   end
 
     private
@@ -49,7 +30,7 @@ class Api::V1::CartsController < ApplicationController
       test_user = User.first
       if test_user.cart.blank?
         # カートを作成
-        Cart.create(user_id:test_user.id, total_price: @ordered_food.price * params[:count])
+        test_user.cart.create(total_price: @ordered_food.price * params[:count])
       end
       # カート詳細情報を作成
       test_user.cart.cart_details.create(
