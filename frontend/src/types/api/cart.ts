1ae7d0b3c3d465b69015e2f7d351ad5a56d00ba2
takamuu <mr.takamuu@gmail.com>
2021-12-17 00:24:11 +0900

    Add cart type #41

--- frontend/src/types/api/cart.ts
+++ frontend/src/types/api/cart.ts
@@ -2,7 +2,9 @@ export type Cart = {
   id: number;
   user_id: number | null;
   total_price: number;
-  food_id: number;
+  name: string;
+  count: number;
+  price: number;
   created_at: string;
   updated_at: string;
 };
