26aaeb34f5345d59e8b5ac63af723947f33d7b87
takamuu <mr.takamuu@gmail.com>
2021-12-17 11:29:39 +0900

    Determine if ordered food is in cart_details #41

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -51,10 +51,18 @@ class Api::V1::CartsController < ApplicationController
         # カート詳細のフード注文個数を更新
         users_cart = @test_user.cart
         cart_details = CartDetail.find_by(food_id: ordered_food.id, cart_id: users_cart.id)
-            # binding.pry
-        cart_details.attributes = {
-          count: cart_details.count + params[:count]
-        }
+        # カート詳細のフード有無を判定
+        if cart_details.blank?
+          cart_details = CartDetail.new(
+            food_id: ordered_food.id,
+            cart_id: users_cart.id,
+            count: params[:count]
+          )
+        else
+          cart_details.attributes = {
+            count: cart_details.count + params[:count]
+          }
+        end
         # カートの注文合計金額を更新
         all_details_of_cart = CartDetail.where(cart_id: users_cart.id)
         total_price = 0
@@ -73,12 +81,4 @@ class Api::V1::CartsController < ApplicationController
       end
      
     end
-
-
-    # else
-    #  ordered_food.new(
-    #   count: params[:count],
-    #   restaurant: ordered_food.restaurant,
-    # )
-    # end
 end
