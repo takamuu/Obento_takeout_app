9cae61be7b12963715982ee85b90330d9ede7878
takamuu <mr.takamuu@gmail.com>
2021-12-23 18:17:41 +0900

    Add routes #50

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
       resources :orders, only: %i[create]
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
