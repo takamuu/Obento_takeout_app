bf728607369cdc8267549423612ff5d921b273cf
takamuu <mr.takamuu@gmail.com>
2021-12-23 18:18:38 +0900

    Add fallback_index-html in application_controller #50

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::API
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  include ActionController::MimeResponds
+  
+  def fallback_index_html
+      respond_to do |format|
+          format.html { render body: Rails.root.join('public/index.html').read }
+      end
+  end
 end
