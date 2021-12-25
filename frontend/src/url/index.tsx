6fae917b0fbc95a8b393178f96544612ae4431d3
takamuu <mr.takamuu@gmail.com>
2021-12-25 20:32:16 +0900

    Change index.tsx #50

--- frontend/src/url/index.tsx
+++ frontend/src/url/index.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable arrow-body-style */
-// const DEFAULT_API_LOCALHOST = 'http://localhost:3000/api/v1';
+const DEFAULT_API_LOCALHOST = `${process.env.REACT_APP_SERVER_URL}/api/v1`;
 
-export const restaurantsIndexUrl = `${process.env.REACT_APP_SERVER_URL}/restaurants`;
+export const restaurantsIndexUrl = `${DEFAULT_API_LOCALHOST}/restaurants`;
 
 export const foodsIndexUrl = (restaurantId: string) =>
-  `${process.env.REACT_APP_SERVER_URL}/restaurants/${restaurantId}/foods`;
+  `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
 
-export const cartsPostUrl = `${process.env.REACT_APP_SERVER_URL}/carts`;
+export const cartsPostUrl = `${DEFAULT_API_LOCALHOST}/carts`;
 
 // todo: ユーザーIDを判定してindexを取得する際に、下記を使用
-export const cartsIndexUrl = () => `${process.env.REACT_APP_SERVER_URL}/carts`;
+export const cartsIndexUrl = () => `${DEFAULT_API_LOCALHOST}/carts`;
 
-export const cartsReplaceUrl = `${process.env.REACT_APP_SERVER_URL}/carts/replace`;
+export const cartsReplaceUrl = `${DEFAULT_API_LOCALHOST}/carts/replace`;
 
-export const orders = `${process.env.REACT_APP_SERVER_URL}/orders`;
+export const orders = `${DEFAULT_API_LOCALHOST}/orders`;
