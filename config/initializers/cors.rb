17d67efcb071fbe7911b13e3720847145c875c4a
takamuu <mr.takamuu@gmail.com>
2021-12-25 20:08:52 +0900

    Change cors.rb #50

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', 'https://obento-takeout-app.herokuapp.com/api'
+    origins 'http://localhost:3001', 'https://obento-takeout-app.herokuapp.com'
 
     resource '*',
       headers: :any,
