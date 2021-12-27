755dcea8675f76ba94a4e4fa461f081cfdcaf99e
takamuu <mr.takamuu@gmail.com>
2021-12-27 18:53:46 +0900

    Add auth routing #6

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,27 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      mount_devise_token_auth_for 'User', at: 'auth'
+
+      resources :test, only: %i[index]
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
+
       resources :restaurants do
         resources :foods, only: %i[index]
       end
+
       resources :carts, only: %i[index create]
+
       put 'carts/replace', to: 'carts#replace'
+      
       resources :orders, only: %i[create]
+      
     end
   end
 
