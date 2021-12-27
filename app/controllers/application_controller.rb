930c639da498a53b40aef11c65536b7f329ff7b5
takamuu <mr.takamuu@gmail.com>
2021-12-28 07:39:51 +0900

    Commented out skip_before_action and helper_method #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
-  skip_before_action :verify_authenticity_token
-  helper_method :current_user, :user_signed_in?
+  # skip_before_action :verify_authenticity_token
+  # helper_method :current_user, :user_signed_in?
 
   include ActionController::MimeResponds
   
