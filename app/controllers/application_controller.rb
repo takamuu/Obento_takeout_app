956992b4dab7c7bce6b8121fee5827254fd21288
takamuu <mr.takamuu@gmail.com>
2021-12-27 18:20:30 +0900

    Add skip_before_action and helper_method to ApplicationController #6

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  skip_before_action :verify_authenticity_token
+  helper_method :current_user, :user_signed_in?
+
   include ActionController::MimeResponds
   
   def fallback_index_html
