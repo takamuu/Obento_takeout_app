d37e38c19164813695c8a63969ff861ffb48111c
takamuu <mr.takamuu@gmail.com>
2022-01-05 09:16:04 +0900

    Add nickname and status #59

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,8 @@ FactoryBot.define do
     password { Faker::Internet.password(min_length: 8) }
     name { Faker::Name.name }
     kana { "カナテスト" }
-    # nickname { "test" }
+    nickname { "test" }
     phone_number { "000-0000-0000" }
-    # status { 1 }
+    status { 1 }
   end
 end
