1cebb42022505577c6e31819772e25d11abed19b
takamuu <mr.takamuu@gmail.com>
2022-01-08 12:57:43 +0900

    Set Cookies #59

--- frontend/src/hooks/useAuth.ts
+++ frontend/src/hooks/useAuth.ts
@@ -8,11 +8,12 @@ import { User } from 'types/api/user';
 import { useMessage } from './useMessage';
 import { SignInParams } from 'types/api/sign';
 import { signInUrl } from 'url/index';
+import { useLoginUser } from './useLoginUser';
 
 export const useAuth = () => {
   const history = useHistory();
   const { showMessage } = useMessage();
-
+  const { setLoginUser } = useLoginUser();
   const [loading, setLoading] = useState(false);
 
   const login = useCallback(
@@ -22,20 +23,23 @@ export const useAuth = () => {
         .post<User>(signInUrl, params)
         .then((res) => {
           showMessage({ title: 'ログインしました', status: 'success' });
+          // ログインに成功した場合はCookieに各値を格納
+          Cookies.set('_access_token', res.headers['access-token']);
+          Cookies.set('_client', res.headers['client']);
+          Cookies.set('_uid', res.headers['uid']);
+          setLoading(false);
           history.push('/restaurants');
         })
         .catch((res) => {
-          console.log(res);
-          console.log(params);
           showMessage({
             title:
               'ユーザID、パスワードの入力に誤りがあるか登録されていません。',
             status: 'error',
           });
-        })
-        .finally(() => setLoading(false));
+          setLoading(false);
+        });
     },
-    [history, showMessage]
+    [history, showMessage, setLoginUser]
   );
   return { login, loading };
 };
