62fcb2372e2d8c4e678c7371522522d2547dcd36
takamuu <mr.takamuu@gmail.com>
2022-01-22 07:41:46 +0900

    Refactoring carts_controller #44

--- app/controllers/api/v1/carts_controller.rb
+++ app/controllers/api/v1/carts_controller.rb
@@ -18,13 +18,13 @@ module Api
         # カートが作成されている場合
         if current_api_v1_user.cart.present?
           # 1.追加するフードを含むカート詳細を取得
-          cart_details = acquire_cart_details(@ordered_food)
+          cart_details = Cart.acquire_cart_details(current_api_v1_user, @ordered_food)
 
           # 2.カート詳細があれば更新、なければ作成
-          update_or_create_cart_details(cart_details)
+          Cart.update_or_create_cart_details(cart_details, current_api_v1_user, cart_details_params)
 
           # 3.カートの合計金額を更新
-          current_api_v1_user.cart.attributes = { total_price: calc_total_price }
+          current_api_v1_user.cart.attributes = { total_price: Cart.calc_total_price(current_api_v1_user) }
           if current_api_v1_user.cart.save
             cart_info = current_api_v1_user.cart.user_has_cart_info
             render json: cart_info, status: :ok
@@ -34,9 +34,9 @@ module Api
 
         else
           # カートが作成されていない場合は、カートを作成
-          new_cart = Cart.create!(user_id: current_api_v1_user.id, total_price: @ordered_food.price * params[:count].to_i)
+          new_cart = Cart.create_cart(current_api_v1_user, @ordered_food, cart_details_params)
           # カート詳細情報を作成
-          new_cart_details = new_cart.cart_details.new(food_id: params[:food_id].to_i, count: params[:count].to_i)
+          new_cart_details = new_cart.cart_details.new(cart_details_params)
           if new_cart_details.save
             cart_info = new_cart.user_has_cart_info
             render json: cart_info, status: :ok
@@ -53,27 +53,8 @@ module Api
           @ordered_food = Food.find(params[:food_id].to_i)
         end
 
-        # 追加するフードを含むカート詳細を取得
-        def acquire_cart_details(ordered_food)
-          current_api_v1_user.cart.cart_details.find_by(food_id: ordered_food.id)
-        end
-
-        # カート詳細があれば更新、なければ作成
-        def update_or_create_cart_details(cart_details)
-          if cart_details.present?
-            cart_details.update!(count: cart_details.count + params[:count].to_i)
-          else
-            current_api_v1_user.cart.cart_details.create!(food_id: params[:food_id].to_i, count: params[:count].to_i)
-          end
-        end
-
-        # カートの合計金額を更新
-        def calc_total_price
-          @total_price = 0
-          current_api_v1_user.cart.cart_details.each do |detail|
-            @total_price += detail.food.price * detail.count
-          end
-          @total_price
+        def cart_details_params
+          params.permit(:food_id, :count)
         end
     end
   end
