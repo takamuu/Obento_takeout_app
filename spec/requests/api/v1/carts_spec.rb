8c55acad6845ee6a9280d5dd8be93b8f74f4d674
takamuu <mr.takamuu@gmail.com>
2022-01-22 02:55:14 +0900

    Create carts_spec #74

--- spec/requests/api/v1/carts_spec.rb
+++ spec/requests/api/v1/carts_spec.rb
@@ -6,24 +6,8 @@ RSpec.describe "Api::V1::Carts", type: :request do
   let(:user) { create(:user) }
 
   describe "GET #index" do
-    subject { get(api_v1_carts_path, headers: headers) }
-
-    context "トークン認証情報がない場合" do
-      subject { get(api_v1_carts_path) }
-
-      it "認証不可(401)がレスポンスされる" do
-        subject
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
-
     context "トークン認証情報がある場合" do
-      context "カートが存在しない場合" do
-        it "no_content(204)がレスポンスされる" do
-          subject
-          expect(response).to have_http_status(:no_content)
-        end
-      end
+      subject { get(api_v1_carts_path, headers: headers) }
 
       context "カートが存在する場合 && 商品が存在する場合" do
         before {
@@ -42,28 +26,111 @@ RSpec.describe "Api::V1::Carts", type: :request do
           expect(json[0]["name"]).to be_present
           expect(json[0]["count"]).to be_present
           expect(json[0]["price"]).to be_present
-          # expect(json["tota_price"]).to be_present
         end
       end
 
       context "カートが存在する場合 && 商品が存在しない場合" do
         before { create(:cart, user_id: current_user.id) }
 
-        xit "no_content(204)がレスポンスされる" do
-          # TODO
+        it "no_content(204)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:no_content)
         end
+      end
+
+      context "カートが存在しない場合" do
+        it "no_content(204)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:no_content)
+        end
+      end
+    end
+
+    context "トークン認証情報がない場合" do
+      subject { get(api_v1_carts_path) }
+
+      it "認証不可(401)がレスポンスされる" do
+        subject
+        expect(response).to have_http_status(:unauthorized)
       end
     end
   end
 
   describe "POST #create" do
-    subject { post(api_v1_carts_path, params: cart_detail_params, headers: headers) }
-    # let(:cart_params) { { cart: attributes_for(:cart_detail_params, user_id: current_user.id)}}
+    let(:food_params) { { "food_id" => @cart_details.food.id, "restaurant_id" => 1, "count" => 1 } }
 
-    let(:cart_detail_params) { { cart_detail: attributes_for(:cart_detail, user_id: current_user.id) } }
+    context "トークン認証情報がある場合" do
+      subject { post(api_v1_carts_path, params: food_params, headers: headers) }
+
+      context "カートが存在する場合 && 商品が存在する場合" do
+        before {
+          @cart = create(:cart, user_id: current_user.id)
+          @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
+        }
+
+        it "ok(200)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "カート情報が更新される" do
+          expect { subject }.to change { @cart.reload.total_price }.from(0).to(2000)
+        end
+
+        it "カート詳細情報が更新される" do
+          expect { subject }.to change { CartDetail.last.count }.by(1)
+        end
+      end
+
+      context "カートが存在する場合 && 商品が存在しない場合" do
+        before {
+          @food = create(:food)
+          @cart = create(:cart, user_id: current_user.id)
+          @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
+        }
+
+        let(:food_params) { { "food_id" => @food.id, "restaurant_id" => 1, "count" => 1 } }
+
+        it "ok(200)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "カートが更新される" do
+          expect { subject }.to change { @cart.reload.total_price }.from(0).to(2000)
+        end
+
+        it "カート詳細が作成される" do
+          expect { subject }.to change { CartDetail.count }.by(1)
+        end
+      end
+
+      context "カートが存在しない場合" do
+        before { @food = create(:food) }
+
+        let(:food_params) { { "food_id" => @food.id, "restaurant_id" => 1, "count" => 1 } }
+
+        it "ok(200)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "カートが作成される" do
+          expect { subject }.to change { Cart.count }.by(1)
+        end
+
+        it "カート詳細情報が作成される" do
+          expect { subject }.to change { CartDetail.count }.by(1)
+        end
+      end
+    end
 
     context "トークン認証情報がない場合" do
-      subject { post(api_v1_carts_path) }
+      subject { post(api_v1_carts_path, params: food_params) }
+
+      before { @food = create(:food) }
+
+      let(:food_params) { { "food_id" => @food.id, "restaurant_id" => 1, "count" => 1 } }
 
       it "認証不可(401)がレスポンスされる" do
         subject
@@ -71,27 +138,7 @@ RSpec.describe "Api::V1::Carts", type: :request do
       end
     end
 
-    context "トークン認証情報がある && カートが存在しない" do
-      it "カートとカート詳細情報を作成する" do
-        subject
-        expect(response).to have_http_status(:ok)
-        expect(response.body).to eq ""
-      end
+    xdescribe "PUT /replace" do
     end
-
-    context "トークン認証情報がある && カートが存在する" do
-      # let!(:cart) { create(:cart, user_id: current_user.id) }
-      # let!(:cart_detail) { create(:cart_detail, cart_id: current_user.cart.id) }
-      it "カートとカート詳細情報を正常に更新する" do
-        subject
-        json = JSON.parse(response.body)
-        expect(response).to have_http_status(:ok)
-        expect(json.size).to eq 1
-        expect(json.sum {|c| c["price"] }).to eq current_user.cart.total_price
-      end
-    end
-  end
-
-  xdescribe "PUT /replace" do
   end
 end
