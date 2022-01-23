36cbc86ae7dc9b8f0f2c9e6471a556c5ae6c3675
takamuu <mr.takamuu@gmail.com>
2022-01-24 06:06:07 +0900

    Set up a LoginUserProvider in Router #67

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -6,42 +6,45 @@ import { loginRoutes } from './LoginRoutes';
 import { Page404 } from 'components/pages/Page404';
 import { restaurantsRoutes } from './RestaurantsRoutes';
 import { HeaderLayout } from 'components/templates/HeaderLayout';
+import { LoginUserProvider } from 'providers/LoginUserProvider';
 
 export const Router: VFC = memo(() => {
   return (
     <Switch>
-      <Route
-        path="/login"
-        render={({ match: { url } }) => (
-          <Switch>
-            {loginRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                {route.children}
-              </Route>
-            ))}
-          </Switch>
-        )}
-      />
-      <Route
-        path="/restaurants"
-        render={({ match: { url } }) => (
-          <Switch>
-            {restaurantsRoutes.map((route) => (
-              <Route
-                key={route.path}
-                exact={route.exact}
-                path={`${url}${route.path}`}
-              >
-                <HeaderLayout>{route.children}</HeaderLayout>
-              </Route>
-            ))}
-          </Switch>
-        )}
-      />
+      <LoginUserProvider>
+        <Route
+          path="/login"
+          render={({ match: { url } }) => (
+            <Switch>
+              {loginRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  {route.children}
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
+        <Route
+          path="/restaurants"
+          render={({ match: { url } }) => (
+            <Switch>
+              {restaurantsRoutes.map((route) => (
+                <Route
+                  key={route.path}
+                  exact={route.exact}
+                  path={`${url}${route.path}`}
+                >
+                  <HeaderLayout>{route.children}</HeaderLayout>
+                </Route>
+              ))}
+            </Switch>
+          )}
+        />
+      </LoginUserProvider>
       <Route>
         <Page404 />
       </Route>
