27cb40c02d9e88de1ebd30029f05b1945f7d8220
takamuu <mr.takamuu@gmail.com>
2022-01-26 05:40:06 +0900

    Add new_registration path to LoginRoutes.tsx #62

--- frontend/src/router/LoginRoutes.tsx
+++ frontend/src/router/LoginRoutes.tsx
@@ -2,12 +2,18 @@ import { Login } from 'components/pages/Login';
 import { UserManagement } from 'components/pages/UserManagement';
 import { Setting } from 'components/pages/Setting';
 import { Page404 } from 'components/pages/Page404';
+import { NewRegistration } from 'components/pages/NewRegistration';
 
 export const loginRoutes = [
   {
     path: '/',
     exact: true,
     children: <Login />,
+  },
+  {
+    path: '/new_registration',
+    exact: false,
+    children: <NewRegistration />,
   },
   {
     path: '/user_management',
