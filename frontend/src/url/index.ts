3362bfa6af814e8e177d922e11c908b02b278cc4
takamuu <mr.takamuu@gmail.com>
2022-01-25 22:28:21 +0900

    Fix index.ts #62

--- frontend/src/url/index.ts
+++ frontend/src/url/index.ts
@@ -10,10 +10,7 @@ export const restaurantsIndexUrl = `${DEFAULT_API_LOCALHOST}/restaurants`;
 export const foodsIndexUrl = (restaurantId: string) =>
   `${DEFAULT_API_LOCALHOST}/restaurants/${restaurantId}/foods`;
 
-export const cartsPostUrl = `${DEFAULT_API_LOCALHOST}/carts`;
-
-// todo: ユーザーIDを判定してindexを取得する際に、下記を使用
-export const cartsIndexUrl = () => `${DEFAULT_API_LOCALHOST}/carts`;
+export const cartsUrl = `${DEFAULT_API_LOCALHOST}/carts`;
 
 export const cartsReplaceUrl = `${DEFAULT_API_LOCALHOST}/carts/replace`;
 
