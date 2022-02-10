6e5f163d77e5a5a644f4b57ffe70c2f8abdd8288
takamuu <mr.takamuu@gmail.com>
2022-02-10 23:47:33 +0900

    Fix contact page #96

--- frontend/src/components/pages/Contact.tsx
+++ frontend/src/components/pages/Contact.tsx
@@ -20,8 +20,8 @@ export const Contact: VFC = memo(() => {
     setContactTitle(e.target.value);
 
   return (
-    <Flex bg="gray.200" align="center" justify="center" height="130vh">
-      <Box bg="white" p={2} borderRadius="md" shadow="md">
+    <Flex bg="gray.200" align="center" justify="center" height="70vh">
+      <Box bg="white" w={'md'} h={'lg'} p={2} borderRadius="md" shadow="md">
         <VStack
           paddingTop="3"
           fontSize="23px"
@@ -44,88 +44,9 @@ export const Contact: VFC = memo(() => {
           />
           <Text h="2">お問い合わせ内容（必須）</Text>
           <Textarea
+            h={'28'}
             borderColor="gray.300"
             placeholder="お問い合わせ内容を入力してください"
-            _placeholder={{ color: 'gray.300' }}
-            _hover={{ color: 'gray.600' }}
-          />
-          <Text h="2">氏名（必須）</Text>
-          <HStack>
-            <Box>
-              <Text h="2" w="32px" textAlign={'center'}>
-                姓
-              </Text>
-            </Box>
-            <Input
-              borderColor="gray.300"
-              placeholder="姓を入力してください"
-              _placeholder={{ color: 'gray.300' }}
-              _hover={{ color: 'gray.600' }}
-            />
-            <Box>
-              <Text h="2" w="32px" textAlign={'center'}>
-                名
-              </Text>
-            </Box>
-            <Input
-              borderColor="gray.300"
-              placeholder="名を入力してください"
-              _placeholder={{ color: 'gray.300' }}
-              _hover={{ color: 'gray.600' }}
-            />
-          </HStack>
-          <Text h="2">フリガナ（必須）</Text>
-          <HStack>
-            <Box>
-              <Text h="2" w="32px" textAlign={'center'} fontSize={'sm'}>
-                セイ
-              </Text>
-            </Box>
-            <Input
-              borderColor="gray.300"
-              placeholder="セイを入力してください"
-              _placeholder={{ color: 'gray.300' }}
-              _hover={{ color: 'gray.600' }}
-            />
-            <Box>
-              <Text h="2" w="32px" textAlign={'center'} fontSize={'sm'}>
-                メイ
-              </Text>
-            </Box>
-            <Input
-              borderColor="gray.300"
-              placeholder="メイを入力してください"
-              _placeholder={{ color: 'gray.300' }}
-              _hover={{ color: 'gray.600' }}
-            />
-          </HStack>
-          <Text h="2">Eメールアドレス（必須）</Text>
-          <Input
-            borderColor="gray.300"
-            placeholder="Eメールを入力してください"
-            _placeholder={{ color: 'gray.300' }}
-            _hover={{ color: 'gray.600' }}
-            type="email"
-          />
-          <Text h="2">Eメールアドレス確認用（必須）</Text>
-          <Input
-            borderColor="gray.300"
-            placeholder="確認用のEメールアドレスを入力してください"
-            _placeholder={{ color: 'gray.300' }}
-            _hover={{ color: 'gray.600' }}
-            type="email"
-          />
-          <Text h="2">住所（任意）</Text>
-          <Input
-            borderColor="gray.300"
-            placeholder="住所を入力してください"
-            _placeholder={{ color: 'gray.300' }}
-            _hover={{ color: 'gray.600' }}
-          />
-          <Text h="2">電話番号（任意）</Text>
-          <Input
-            borderColor="gray.300"
-            placeholder="電話番号を入力してください"
             _placeholder={{ color: 'gray.300' }}
             _hover={{ color: 'gray.600' }}
           />
@@ -147,7 +68,9 @@ export const Contact: VFC = memo(() => {
             </Checkbox>
           </VStack>
           <Spacer />
-          <Button>送信する</Button>
+          <Button bg="brand" color="white" _hover={{ opacity: 0.8 }}>
+            送信する
+          </Button>
         </Stack>
       </Box>
     </Flex>
