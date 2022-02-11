656b27358ca7f2d6c5c7aeaba4db36c39cd28774
takamuu <mr.takamuu@gmail.com>
2022-02-11 15:06:06 +0900

    Add each pages path to RestaurantsRoutes #101

--- frontend/src/router/RestaurantsRoutes.tsx
+++ frontend/src/router/RestaurantsRoutes.tsx
@@ -3,6 +3,12 @@ import { Foods } from 'components/pages/Foods';
 import { Cart } from 'components/pages/Cart';
 import { Page404 } from 'components/pages/Page404';
 import { Contact } from 'components/pages/Contact';
+import { HowToUseBenteku } from 'components/pages/HowToUseBenteku';
+import { MyPage } from 'components/pages/MyPage';
+import { TermsOfUse } from 'components/pages/TermsOfUse';
+import { Policy } from 'components/pages/Policy';
+import { CommercialTransactionsLaw } from 'components/pages/CommercialTransactionsLaw';
+import { PurchaseHistory } from 'components/pages/PurchaseHistory';
 
 export const restaurantsRoutes = [
   {
@@ -24,6 +30,36 @@ export const restaurantsRoutes = [
     path: 'contact',
     exact: false,
     children: <Contact />,
+  },
+  {
+    path: 'how_to_use_benteku',
+    exact: false,
+    children: <HowToUseBenteku />,
+  },
+  {
+    path: 'my_page',
+    exact: false,
+    children: <MyPage />,
+  },
+  {
+    path: 'policy',
+    exact: false,
+    children: <Policy />,
+  },
+  {
+    path: 'commercial_transactions_law',
+    exact: false,
+    children: <CommercialTransactionsLaw />,
+  },
+  {
+    path: 'terms_of_use',
+    exact: false,
+    children: <TermsOfUse />,
+  },
+  {
+    path: 'purchase_history',
+    exact: false,
+    children: <PurchaseHistory />,
   },
   {
     path: '*',
