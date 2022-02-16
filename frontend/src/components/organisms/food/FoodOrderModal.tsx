e9d26f6c18e1dcccb6c05159f5cf06eb15751f54
takamuu <mr.takamuu@gmail.com>
2022-02-16 12:37:36 +0900

    Add Disclosure and CartModal to FoodOrderModal #46

--- frontend/src/components/organisms/food/FoodOrderModal.tsx
+++ frontend/src/components/organisms/food/FoodOrderModal.tsx
@@ -18,12 +18,13 @@ import { CartButton } from 'components/atoms/button/CartButton';
 import { Food } from 'types/api/food';
 import { Image } from '@chakra-ui/image';
 import { usePostCart } from 'hooks/usePostCart';
+import { CartModal } from '../cart/CartModal';
+import { useDisclosure } from '@chakra-ui/react';
 
 type Props = {
   food: Food;
   isOpen: boolean;
   onClose: () => void;
-  // onClickCart: (countNumber: number) => void;
 };
 
 export const FoodOrderModal: VFC<Props> = memo((props) => {
@@ -35,80 +36,80 @@ export const FoodOrderModal: VFC<Props> = memo((props) => {
 
   const onClickDownCount = () => setCount(count - 1);
 
-  const onCloseModal = () => {
-    setCount(1);
-    onClose();
-  };
-
   const { postCart } = usePostCart();
 
-  // const {
-  //   isOpen: isOpenCartModal,
-  //   onOpen: onOpenCartModal,
-  //   onClose: onCloseCartModal,
-  // } = useDisclosure();
-
-  const onClickCart = useCallback(({ food, count }) => {
+  const onClickCartButton = useCallback(({ food, count }) => {
     postCart({ food: food, count: count });
-    onCloseModal();
-    // onOpenCartModal();
+    setCount(1);
+    onClose();
+    onOpenCart();
   }, []);
 
+  // For CartModal
+  const {
+    isOpen: isOpenCart,
+    onOpen: onOpenCart,
+    onClose: onCloseCart,
+  } = useDisclosure();
+
   return (
-    <Modal
-      size="lg"
-      isOpen={isOpen}
-      onClose={onCloseModal}
-      autoFocus={false}
-      motionPreset="slideInBottom"
-    >
-      <ModalOverlay>
-        <ModalContent bg="white">
-          <ModalCloseButton
-            bgColor="white"
-            rounded="full"
-            _hover={{ opacity: 0.8 }}
-          />
-          <ModalBody mx={2}>
-            <Stack spacing={2}>
-              <FormControl>
-                <Image src={food?.image} />
-              </FormControl>
-              <FormControl>
-                <Text fontSize="xl">{food?.name}</Text>
-              </FormControl>
-              <FormControl>
-                <Text fontSize="xl">{food?.food_description}</Text>
-              </FormControl>
-              <FormControl>
-                <Text fontSize="xl">金額 ¥ {food?.price.toLocaleString()}</Text>
-              </FormControl>
-            </Stack>
-          </ModalBody>
-          <ModalFooter>
-            <CountDownButton
-              onClick={() => onClickDownCount()}
-              isDisabled={count <= 1}
+    <>
+      <Modal
+        size="lg"
+        isOpen={isOpen}
+        onClose={onClose}
+        autoFocus={false}
+        motionPreset="slideInBottom"
+      >
+        <ModalOverlay>
+          <ModalContent bg="white">
+            <ModalCloseButton
+              bgColor="white"
+              rounded="full"
+              _hover={{ opacity: 0.8 }}
             />
-            <Text fontSize="xl" p={4}>
-              {count}
-            </Text>
-            <CountUpButton
-              onClick={() => onClickUpCount()}
-              isDisabled={count >= 9}
-            />
-            <Spacer />
-            <CartButton onClick={() => onClickCart({ food, count })}>
-              <Text p={2}>{`${count}点をカートに追加 `}</Text>
-              <Text p={2}>{`¥${(count * food?.price).toLocaleString()}`}</Text>
-            </CartButton>
-            {/* <CartModal
-              isOpenCartModal={isOpenCartModal}
-              onCloseCartModal={onCloseCartModal}
-            /> */}
-          </ModalFooter>
-        </ModalContent>
-      </ModalOverlay>
-    </Modal>
+            <ModalBody mx={2}>
+              <Stack spacing={2}>
+                <FormControl>
+                  <Image src={food?.image} />
+                </FormControl>
+                <FormControl>
+                  <Text fontSize="xl">{food?.name}</Text>
+                </FormControl>
+                <FormControl>
+                  <Text fontSize="xl">{food?.food_description}</Text>
+                </FormControl>
+                <FormControl>
+                  <Text fontSize="xl">
+                    金額 ¥ {food?.price.toLocaleString()}
+                  </Text>
+                </FormControl>
+              </Stack>
+            </ModalBody>
+            <ModalFooter>
+              <CountDownButton
+                onClick={() => onClickDownCount()}
+                isDisabled={count <= 1}
+              />
+              <Text fontSize="xl" p={4}>
+                {count}
+              </Text>
+              <CountUpButton
+                onClick={() => onClickUpCount()}
+                isDisabled={count >= 9}
+              />
+              <Spacer />
+              <CartButton onClick={() => onClickCartButton({ food, count })}>
+                <Text p={2}>{`${count}点をカートに追加 `}</Text>
+                <Text p={2}>{`¥${(
+                  count * food?.price
+                ).toLocaleString()}`}</Text>
+              </CartButton>
+            </ModalFooter>
+          </ModalContent>
+        </ModalOverlay>
+      </Modal>
+      <CartModal isOpen={isOpenCart} onClose={onCloseCart} />
+    </>
   );
 });
