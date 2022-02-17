d3b5c23cfbf61e4664177db3c04ebce1298283b9
takamuu <mr.takamuu@gmail.com>
2022-02-17 20:28:20 +0900

    Fix Cart page #46

--- frontend/src/components/pages/Cart.tsx
+++ frontend/src/components/pages/Cart.tsx
@@ -1,15 +1,24 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable arrow-body-style */
-import { Center, Heading, Wrap, WrapItem } from '@chakra-ui/layout';
+import { Box, Center, Text, VStack, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
+import { CartButton } from 'components/atoms/button/CartButton';
 import { CartCard } from 'components/organisms/cart/CartCard';
 import { useCartIndex } from 'hooks/useCartIndex';
-import { memo, useEffect, VFC } from 'react';
+import { memo, useCallback, useEffect, VFC } from 'react';
+import { useHistory } from 'react-router-dom';
 
 export const Cart: VFC = memo(() => {
   const { getCarts, carts, loading } = useCartIndex();
 
-  useEffect(() => getCarts(), []);
+  useEffect(() => {
+    getCarts();
+  }, []);
+
+  const history = useHistory();
+  const onClickOrderButton = useCallback(() => {
+    alert('stripe決済ページを飛ばして受取票ページへ遷移');
+  }, []);
 
   return (
     <>
@@ -18,25 +27,34 @@ export const Cart: VFC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap>
-          <Heading p={4}>カートページ</Heading>
-          <Wrap p={{ base: 4, md: 10 }} justify="space-around">
+        <Wrap justify={'center'}>
+          <Box fontSize={'4xl'} p={4} w={'full'} textAlign={'center'}>
+            カートページ
+          </Box>
+          <Wrap p={{ base: 4, md: 10 }}>
             {carts ? (
               <>
-                {carts.map((cart) => (
-                  <WrapItem key={cart.id}>
-                    <CartCard
-                      foodName={cart.name}
-                      count={cart.count}
-                      price={cart.price}
-                    />
-                  </WrapItem>
-                ))}
+                <VStack>
+                  {carts.map((cart) => (
+                    <WrapItem key={cart.id}>
+                      <CartCard
+                        foodName={cart.name}
+                        count={cart.count}
+                        price={cart.price}
+                      />
+                    </WrapItem>
+                  ))}
+                </VStack>
               </>
             ) : (
               <p>カートはありません</p>
             )}
           </Wrap>
+          <WrapItem p={10} w={'full'} justifyContent={'center'}>
+            <CartButton onClick={() => onClickOrderButton()}>
+              <Text p={2}>注文する</Text>
+            </CartButton>
+          </WrapItem>
         </Wrap>
       )}
     </>
