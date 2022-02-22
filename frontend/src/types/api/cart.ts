ca5578292864c22f37424bbfd1d186c403a07f5f
takamuu <mr.takamuu@gmail.com>
2022-02-22 12:49:08 +0900

    Fix type Cart #53

--- frontend/src/types/api/cart.ts
+++ frontend/src/types/api/cart.ts
@@ -1,3 +1,16 @@
+export type Food = {
+  id: number;
+  restaurant_id: number;
+  name: string;
+  food_description: string;
+  price: number;
+  sales_limit?: number;
+  sales_status?: string;
+  image?: string;
+  created_at: string;
+  updated_at: string;
+};
+
 export type Cart = {
   id: number;
   user_id: number | null;
@@ -5,18 +18,7 @@ export type Cart = {
   name: string;
   count: number;
   price: number;
-  food?: {
-    id: number;
-    restaurant_id: number;
-    name: string;
-    food_description: string;
-    price: number;
-    sales_limit?: number;
-    sales_status?: string;
-    image?: string;
-    created_at: string;
-    updated_at: string;
-  };
+  food?: Food;
   created_at: string;
   updated_at: string;
 };
