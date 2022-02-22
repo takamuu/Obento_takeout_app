c8ff4c1349315e60f24e7693ec1570a18750eaab
takamuu <mr.takamuu@gmail.com>
2022-02-22 10:10:30 +0900

    Add Add foot type to card type #53

--- frontend/src/types/api/cart.ts
+++ frontend/src/types/api/cart.ts
@@ -5,6 +5,18 @@ export type Cart = {
   name: string;
   count: number;
   price: number;
+  food?: {
+    id: number;
+    restaurant_id: number;
+    name: string;
+    food_description: string;
+    price: number;
+    sales_limit?: number;
+    sales_status?: string;
+    image?: string;
+    created_at: string;
+    updated_at: string;
+  };
   created_at: string;
   updated_at: string;
 };
