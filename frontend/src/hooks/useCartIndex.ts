394c9358f53b074c840b373e6cae3aab04ecc687
takamuu <mr.takamuu@gmail.com>
2022-02-24 22:12:36 +0900

    Move useEffect to custom hooks #78

--- frontend/src/hooks/useCartIndex.ts
+++ frontend/src/hooks/useCartIndex.ts
@@ -2,7 +2,7 @@
 /* eslint-disable arrow-body-style */
 import axios from 'axios';
 import Cookies from 'js-cookie';
-import { useCallback, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Cart } from 'types/api/cart';
 
 import { cartsUrl } from '../url';
@@ -13,26 +13,28 @@ export const useCartIndex = () => {
   const [loading, setLoading] = useState(false);
   const [carts, setCarts] = useState<Array<Cart>>([]);
 
-  const getCarts = useCallback(async () => {
-    setLoading(true);
-    try {
-      const result = await axios.get<Array<Cart>>(cartsUrl, {
-        headers: {
-          'access-token': Cookies.get('_access_token'),
-          client: Cookies.get('_client'),
-          uid: Cookies.get('_uid'),
-        },
-      });
-      setCarts(result.data);
-    } catch (e) {
-      showMessage({
-        title: 'データの取得に失敗しました',
-        status: 'error',
-      });
-    } finally {
-      setLoading(false);
-    }
+  useEffect(() => {
+    const fetchCarts = async () => {
+      setLoading(true);
+      try {
+        const result = await axios.get<Array<Cart>>(cartsUrl, {
+          headers: {
+            'access-token': Cookies.get('_access_token'),
+            client: Cookies.get('_client'),
+            uid: Cookies.get('_uid'),
+          },
+        });
+        setCarts(result.data);
+      } catch (e) {
+        showMessage({
+          title: 'データの取得に失敗しました',
+          status: 'error',
+        });
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchCarts();
   }, []);
-
-  return { getCarts, loading, carts };
+  return { carts, loading };
 };
