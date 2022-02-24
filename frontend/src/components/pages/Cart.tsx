52c621bd68d1a93d8973b7dff61a615634e34015
takamuu <mr.takamuu@gmail.com>
2022-02-25 00:16:03 +0900

    Added a total price calculation function to the cart page #78

--- frontend/src/components/pages/Cart.tsx
+++ frontend/src/components/pages/Cart.tsx
@@ -1,15 +1,30 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable arrow-body-style */
-import { memo, useCallback, useEffect, VFC } from 'react';
+import { memo, useCallback, useEffect, useState, VFC } from 'react';
 import { Box, Center, Text, VStack, Wrap, WrapItem } from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
 
 import { CartButton } from 'components/atoms/button/CartButton';
 import { CartCard } from 'components/organisms/cart/CartCard';
 import { useCartIndex } from 'hooks/useCartIndex';
+import { NewCarts } from 'types/api/newCarts';
 
 export const Cart: VFC = memo(() => {
   const { carts, loading } = useCartIndex();
+  const [newCarts, setNewCarts] = useState<NewCarts>([]);
+
+  useEffect(() => {
+    setNewCarts(
+      carts.map((cart) => ({
+        id: cart.food.id,
+        amount: cart.count * cart.food.price,
+        count: cart.count,
+        name: cart.food.name,
+        price: cart.food.price,
+        food: cart.food,
+      }))
+    );
+  }, [carts]);
 
   const onClickOrderButton = useCallback(() => {
     alert('stripe決済ページを飛ばして受取票ページへ遷移');
@@ -17,9 +32,10 @@ export const Cart: VFC = memo(() => {
 
   // Calculate the total amount
 
-  let totalAmount = 0;
-
-  carts.map((cart) => (totalAmount += cart.count * cart.food.price));
+  const totalAmount = newCarts.reduce(
+    (total, newCart) => total + newCart.amount,
+    0
+  );
 
   return (
     <>
@@ -33,16 +49,28 @@ export const Cart: VFC = memo(() => {
             カートページ
           </Box>
           <Wrap p={{ base: 4, md: 10 }}>
-            {carts ? (
+            {newCarts ? (
               <>
                 <VStack>
-                  {carts.map((cart, i) => (
+                  {newCarts.map((cart, i) => (
                     <WrapItem key={i}>
                       <CartCard
                         food={cart.food}
                         foodName={cart.food.name}
                         count={cart.count}
-                        price={cart.food.price}
+                        price={cart.price}
+                        onChangeCount={(newCount) => {
+                          setNewCarts((s) =>
+                            s.map((newCart) => {
+                              if (newCart.id !== cart.food.id) return newCart;
+                              return {
+                                ...newCart,
+                                amount: cart.food.price * newCount,
+                                count: newCount,
+                              };
+                            })
+                          );
+                        }}
                       />
                     </WrapItem>
                   ))}
