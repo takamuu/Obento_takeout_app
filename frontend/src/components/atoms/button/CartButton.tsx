69d295b55a3e63ccb67bdd0865543e1c0572b8c1
takamuu <mr.takamuu@gmail.com>
2022-03-02 01:46:25 +0900

    Add display to CartButton #120

--- frontend/src/components/atoms/button/CartButton.tsx
+++ frontend/src/components/atoms/button/CartButton.tsx
@@ -5,16 +5,24 @@ type Props = {
   children: ReactNode;
   disabled?: boolean;
   loading?: boolean;
+  display?: string;
   onClick: () => void;
 };
 
 export const CartButton: VFC<Props> = memo((props) => {
-  const { children, disabled = false, loading = false, onClick } = props;
+  const {
+    children,
+    disabled = false,
+    loading = false,
+    display = 'block',
+    onClick,
+  } = props;
   return (
     <Button
       size="lg"
       bg="brand"
       color="white"
+      display={display}
       _hover={{ opacity: 0.8 }}
       disabled={disabled || loading}
       isLoading={loading}
