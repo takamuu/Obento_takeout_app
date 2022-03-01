9c32d2e8604c3efa3b894cf2bc894f42566bbfff
takamuu <mr.takamuu@gmail.com>
2022-03-02 01:43:29 +0900

    Create FoodButton #120

---
+++ frontend/src/components/atoms/button/FoodButton.tsx
@@ -0,0 +1,26 @@
+import { memo, ReactNode, VFC } from 'react';
+import { Button } from '@chakra-ui/button';
+
+type Props = {
+  children: ReactNode;
+  disabled?: boolean;
+  loading?: boolean;
+  onClick: () => void;
+};
+
+export const FoodButton: VFC<Props> = memo((props) => {
+  const { children, disabled = false, loading = false, onClick } = props;
+  return (
+    <Button
+      size="lg"
+      bg="brand"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+      disabled={disabled || loading}
+      isLoading={loading}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+});
