4a7a72c54431b26a10b61c1fe87ef5120394625a
takamuu <mr.takamuu@gmail.com>
2022-03-25 23:22:04 +0900

    Add guest login routes #116

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      devise_scope :api_v1_user do
+        post "auth/guest_sign_in", to: "auth/sessions#guest_sign_in"
+      end
+
       mount_devise_token_auth_for "User", at: "auth", controllers: {
         registrations: "api/v1/auth/registrations",
       }
