612fbe0674ca642d8ba2d52e32951a6c3b00fed9
takamuu <mr.takamuu@gmail.com>
2022-03-26 00:26:58 +0900

    Added onClick and loading to GuestButton #116

--- frontend/src/components/atoms/button/GuestButton.tsx
+++ frontend/src/components/atoms/button/GuestButton.tsx
@@ -3,12 +3,20 @@ import { Button } from '@chakra-ui/button';
 
 type Props = {
   children: ReactNode;
+  loading?: boolean;
+  onClick: () => void;
 };
 
 export const GuestButton: VFC<Props> = memo((props) => {
-  const { children } = props;
+  const { children, loading = false, onClick } = props;
   return (
-    <Button bg="blue.400" color="white" _hover={{ opacity: 0.8 }}>
+    <Button
+      bg="blue.400"
+      color="white"
+      _hover={{ opacity: 0.8 }}
+      isLoading={loading}
+      onClick={onClick}
+    >
       {children}
     </Button>
   );
