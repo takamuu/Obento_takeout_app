10a1f5fb2a27bc5620b2f6f2abca98c5a4e66fc4
takamuu <mr.takamuu@gmail.com>
2022-03-30 16:21:32 +0900

    Create order.rb #47

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,8 @@
 class Order < ApplicationRecord
+  require "date"
+  require "securerandom"
+  require "bigdecimal"
+
   belongs_to :user
   has_many :order_details, dependent: :destroy
   has_many :foods, through: :order_details
@@ -13,4 +17,29 @@ class Order < ApplicationRecord
     complete: 1,
     delivered: 2,
   }
+
+  # rubocop:disable all
+  def self.create_order_history(user)
+    ActiveRecord::Base.transaction do
+      # 注文履歴を作成
+      user.orders.create!(
+        user_id: user.id,
+        rceipt_number: "#{Date.today.day} #{SecureRandom.alphanumeric(3)}",
+        total_price: user.cart.total_price,
+        consumption_tax: (BigDecimal(user.cart.total_price) * BigDecimal(ENV["CONSUMPTION_TAX"])).ceil,
+        progress_status: "orde",
+      )
+      # カート詳細から注文履歴詳細を作成
+      user.cart_details.map do |cart_detail|
+        OrderDetail.create!(
+          order_id: user.orders.last.id,
+          food_id: cart_detail.food_id,
+          count: cart_detail.count,
+        )
+      end
+
+      user.cart.destroy
+    end
+  end
+  # rubocop:disable all
 end
