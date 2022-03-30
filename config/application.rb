a9b3c9a7336a37fbcecf2bb932c9555badc67f0e
takamuu <mr.takamuu@gmail.com>
2022-03-30 15:55:22 +0900

    Added env file setting to application.rb #47

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,9 @@ module ObentoTakeoutApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    if Rails.env.development? || Rails.env.test?
+      Bundler.require(*Rails.groups)
+      Dotenv::Railtie.load
+    end
   end
 end
