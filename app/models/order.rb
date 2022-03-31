95eb4738e75c63ca1562c5bfb63dc01a194b8053
takamuu <mr.takamuu@gmail.com>
2022-03-31 15:53:12 +0900

    Fix order.rb #47

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,7 +5,6 @@ class Order < ApplicationRecord
 
   belongs_to :user
   has_many :order_details, dependent: :destroy
-  has_many :foods, through: :order_details
 
   validates :rceipt_number,   presence: true
   validates :total_price,     presence: true
@@ -18,28 +17,47 @@ class Order < ApplicationRecord
     delivered: 2,
   }
 
-  # rubocop:disable all
+  def self.check_users_order_history?(user)
+    user.orders.present? && user.order_details.present?
+  end
+
+  def self.formatted_parchase_history(user)
+    user.orders
+    user.order_details
+  end
+
+  def self.confirm_cart_presence?(params)
+    users_cart = Cart.find_by(user_id: params[:user_id].to_i)
+    users_cart.present? && CartDetail.find_by(cart_id: users_cart.id).present?
+  end
+
   def self.create_order_history(user)
     ActiveRecord::Base.transaction do
       # 注文履歴を作成
-      user.orders.create!(
-        user_id: user.id,
-        rceipt_number: "#{Date.today.day} #{SecureRandom.alphanumeric(3)}",
-        total_price: user.cart.total_price,
-        consumption_tax: (BigDecimal(user.cart.total_price) * BigDecimal(ENV["CONSUMPTION_TAX"])).ceil,
-        progress_status: "orde",
-      )
+      order_create(user)
       # カート詳細から注文履歴詳細を作成
-      user.cart_details.map do |cart_detail|
-        OrderDetail.create!(
-          order_id: user.orders.last.id,
-          food_id: cart_detail.food_id,
-          count: cart_detail.count,
-        )
-      end
-
+      order_details_create(user)
       user.cart.destroy
     end
   end
-  # rubocop:disable all
+
+  def self.order_create(user)
+    user.orders.create!(
+      user_id: user.id,
+      rceipt_number: "#{Date.today.day} #{SecureRandom.alphanumeric(3)}", # rubocop:disable Rails/Date
+      total_price: user.cart.total_price,
+      consumption_tax: (BigDecimal(user.cart.total_price) * BigDecimal(ENV["CONSUMPTION_TAX"])).ceil,
+      progress_status: "orde",
+    )
+  end
+
+  def self.order_details_create(user)
+    user.cart_details.each do |cart_detail|
+      OrderDetail.create!(
+        order_id: user.orders.last.id,
+        food_id: cart_detail.food_id,
+        count: cart_detail.count,
+      )
+    end
+  end
 end
