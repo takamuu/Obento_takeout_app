ab0a34af8b1b2c3dd2465a193681ae3b5032ca32
takamuu <mr.takamuu@gmail.com>
2022-03-31 15:52:31 +0900

    Fix order_controller.rb #47

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -4,7 +4,7 @@ module Api
       before_action :authenticate_api_v1_user!
 
       def index
-        if current_api_v1_user.orders.present? && current_api_v1_user.order_details.present?
+        if Order.check_users_order_history?(current_api_v1_user)
           render json: current_api_v1_user.orders, status: :ok
         else
           render json: [], status: :no_content
@@ -12,7 +12,8 @@ module Api
       end
 
       def create
-        if Cart.find_by(user_id: order_params[:user_id].to_i).present? && Order.create_order_history(current_api_v1_user).present?
+        if Order.confirm_cart_presence?(order_params)
+          Order.create_order_history(current_api_v1_user)
           render status: :ok
         else
           render status: :no_content
