bafa5430f630319d87b27fcae57bbd440eef76c5
takamuu <mr.takamuu@gmail.com>
2022-04-01 14:09:36 +0900

    Move method from Order model to OrderDetail model #47

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -3,4 +3,14 @@ class OrderDetail < ApplicationRecord
   belongs_to :food
 
   validates :count, presence: true
+
+  def self.order_details_create!(user)
+    user.cart_details.each do |cart_detail|
+      self.create!(
+        order_id: user.orders.last.id,
+        food_id: cart_detail.food_id,
+        count: cart_detail.count,
+      )
+    end
+  end
 end
