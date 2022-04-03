b7577bb0072292b797628c38c62abbd69cb6df42
takamuu <mr.takamuu@gmail.com>
2022-04-04 00:47:12 +0900

    Create OrderSerializer.rb #51

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,8 +1,20 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :rceipt_number, :total_price, :consumption_tax, :progress_status, :created_at
+  attributes :id, :user_id, :rceipt_number, :total_price, :consumption_tax, :progress_status, :restaurant_name, :created_at
   has_many :order_details
 
+  def restaurant_name
+    object.order_details.first.food.restaurant.name
+  end
+
   class OrderDetailSerializer < ActiveModel::Serializer
-    attributes :id, :order_id, :food_id, :count
+    attributes :id, :count, :food_name, :food_price
+
+    def food_name
+      object.food.name
+    end
+
+    def food_price
+      object.food.price
+    end
   end
 end
