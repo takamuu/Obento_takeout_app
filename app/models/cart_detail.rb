0394050aa7b4cec497e0a45e0e240d84cd4f38c5
takamuu <mr.takamuu@gmail.com>
2022-04-08 13:37:24 +0900

    Remove unnecessary code #140

--- app/models/cart_detail.rb
+++ app/models/cart_detail.rb
@@ -3,13 +3,4 @@ class CartDetail < ApplicationRecord
   belongs_to :cart, optional: true
 
   validates :count,  presence: true, numericality: { greater_than: 0 }
-
-  def self.update_cart_details(user, food, food_count)
-    cart_detail = user.cart_details.find_by(food_id: food.id)
-    cart_detail.update!(food_id: food.id, count: cart_detail.count + food_count)
-  end
-
-  def self.create_cart_details(user, food, food_count)
-    create!(food_id: food.id, cart_id: user.cart.id, count: food_count)
-  end
 end
