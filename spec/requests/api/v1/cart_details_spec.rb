95b3319aa816d97d029b53ad70ee40e185ecf088
takamuu <mr.takamuu@gmail.com>
2022-04-10 01:11:11 +0900

    Added replace test #63

--- spec/requests/api/v1/cart_details_spec.rb
+++ spec/requests/api/v1/cart_details_spec.rb
@@ -30,19 +30,6 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
           expect { subject }.to change { @cart.reload.total_price }.from(1000).to(0)
         end
       end
-
-      context "カートが存在する場合 && カート詳細が存在しない場合" do
-        let(:delete_params) { { id: @cart_details.id } }
-        before do
-          create(:cart, user_id: current_user.id)
-          @cart_details = create(:cart_detail)
-        end
-
-        it "no_content(204)がレスポンスされる" do
-          subject
-          expect(response).to have_http_status(:no_content)
-        end
-      end
     end
 
     context "トークン認証情報がない場合" do
@@ -78,25 +65,11 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
         end
 
         it "カート詳細情報が更新されること" do
-          expect { subject }.to change { @cart_details.reload.count }.by(1)
+          expect { subject }.to change { @cart_details.reload.count }.by(2)
         end
 
         it "カートの合計金額が更新されること" do
-          expect { subject }.to change { @cart.reload.total_price }.from(1000).to(2000)
-        end
-      end
-
-      context "カートが存在する場合 && カート詳細が存在しない場合" do
-        let(:update_params) { { cart_detail: { id: nil, count: nil, food_id: nil }, id: 1 } }
-
-        before do
-          @cart = create(:cart, user_id: current_user.id, total_price: 1000)
-          @food = create(:food)
-        end
-
-        it "no_content(204)がレスポンスされる" do
-          subject
-          expect(response).to have_http_status(:no_content)
+          expect { subject }.to change { @cart.reload.total_price }.from(1000).to(3000)
         end
       end
     end
@@ -107,6 +80,53 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
       before do
         create(:cart, user_id: current_user.id)
         @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
+      end
+
+      it "認証不可(401)がレスポンスされる" do
+        subject
+        expect(response).to have_http_status(:unauthorized)
+      end
+    end
+  end
+
+  describe "PUT #replace" do
+    let(:update_params) { { cart_detail: { cart_id: @cart.id, count: 1, food_id: @other_restaurant_food.id }, id: @other_restaurant_food.id } }
+    context "トークン認証情報がある場合" do
+      subject { put(api_v1_path(update_params), headers: headers) }
+
+      context "カートが存在する場合 && カート詳細が存在する場合" do
+        before do
+          @restaurant = create(:restaurant)
+          @first_food = create(:food, restaurant_id: @restaurant.id)
+          @second_food = create(:food, restaurant_id: @restaurant.id)
+          @cart = create(:cart, user_id: current_user.id, total_price: 2000)
+          create(:cart_detail, food_id: @first_food.id, cart_id: @cart.id)
+          create(:cart_detail, food_id: @second_food.id, cart_id: @cart.id)
+          @other_restaurant_food = create(:food)
+        end
+
+        it "ok(200)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:ok)
+        end
+
+        it "存在していたカート詳細が全て削除され、別のレストランのカート詳細が作成されること" do
+          expect { subject }.to change { CartDetail.count }.from(2).to(1)
+        end
+
+        it "カートの合計金額が更新されること" do
+          expect { subject }.to change { @cart.reload.total_price }.from(2000).to(1000)
+        end
+      end
+    end
+
+    context "トークン認証情報がない場合" do
+      subject { put(api_v1_path(update_params)) }
+
+      before do
+        @cart = create(:cart, user_id: current_user.id, total_price: 1000)
+        create(:cart_detail, cart_id: current_user.cart.id)
+        @other_restaurant_food = create(:food)
       end
 
       it "認証不可(401)がレスポンスされる" do
