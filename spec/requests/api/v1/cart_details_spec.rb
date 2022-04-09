8274b02447fa22941e6780c19019e00b60816a50
takamuu <mr.takamuu@gmail.com>
2022-04-09 11:32:51 +0900

    Support change from replace to update #63

--- spec/requests/api/v1/cart_details_spec.rb
+++ spec/requests/api/v1/cart_details_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
     context "トークン認証情報がある場合" do
       subject { delete(api_v1_cart_detail_path(delete_params), headers: headers) }
 
-      context "カートが存在する場合 && 商品が存在する場合" do
-        before {
+      context "カートが存在する場合 && カート詳細が存在する場合" do
+        before do
           @cart = create(:cart, user_id: current_user.id, total_price: 1000)
           @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
-        }
+        end
 
         it "ok(200)がレスポンスされる" do
           subject
@@ -31,15 +31,16 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
         end
       end
 
-      context "カートが存在する場合 && 商品が存在しない場合" do
-        let(:delete_params) { @food }
-        before {
+      context "カートが存在する場合 && カート詳細が存在しない場合" do
+        let(:delete_params) { { id: @cart_details.id } }
+        before do
           create(:cart, user_id: current_user.id)
-          @food = create(:food)
-        }
+          @cart_details = create(:cart_detail)
+        end
 
-        it "カート詳細情報は削除できないこと" do
-          expect { subject }.not_to change { CartDetail.count }
+        it "no_content(204)がレスポンスされる" do
+          subject
+          expect(response).to have_http_status(:no_content)
         end
       end
     end
@@ -59,17 +60,17 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
     end
   end
 
-  describe "PUT #replace" do
-    let(:replace_params) { { cart_detail: { count: 2, food_id: @cart_details.food.id } } }
+  describe "PATCH #update" do
+    let(:update_params) { { cart_detail: { id: @cart_details.id, count: 2, food_id: @cart_details.food.id }, id: @cart_details.food.id } }
 
     context "トークン認証情報がある場合" do
-      subject { put(api_v1_cart_details_replace_path(replace_params), headers: headers) }
+      subject { patch(api_v1_cart_detail_path(update_params), headers: headers) }
 
-      context "カートが存在する場合 && 商品が存在する場合" do
-        before {
+      context "カートが存在する場合 && カート詳細が存在する場合" do
+        before do
           @cart = create(:cart, user_id: current_user.id, total_price: 1000)
           @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
-        }
+        end
 
         it "ok(200)がレスポンスされる" do
           subject
@@ -85,13 +86,13 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
         end
       end
 
-      context "カートが存在する場合 && 商品が存在しない場合" do
-        let(:replace_params) { { cart_detail: { count: 2, food_id: @food.id } } }
+      context "カートが存在する場合 && カート詳細が存在しない場合" do
+        let(:update_params) { { cart_detail: { id: nil, count: nil, food_id: nil }, id: 1 } }
 
-        before {
+        before do
           @cart = create(:cart, user_id: current_user.id, total_price: 1000)
           @food = create(:food)
-        }
+        end
 
         it "no_content(204)がレスポンスされる" do
           subject
@@ -101,12 +102,12 @@ RSpec.describe "Api::V1::CartDetails", type: :request do
     end
 
     context "トークン認証情報がない場合" do
-      subject { put(api_v1_cart_details_replace_path(replace_params)) }
+      subject { patch(api_v1_cart_detail_path(update_params)) }
 
-      before {
+      before do
         create(:cart, user_id: current_user.id)
         @cart_details = create(:cart_detail, cart_id: current_user.cart.id)
-      }
+      end
 
       it "認証不可(401)がレスポンスされる" do
         subject
