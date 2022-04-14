575e8def47c4a47c0914c54e603b17b2457d60c6
takamuu <mr.takamuu@gmail.com>
2022-04-14 23:13:31 +0900

    Fixed factory bot for users #100"

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :user do
-    email { Faker::Internet.unique.email }
+    email { Faker::Internet.unique.free_email }
     password { Faker::Internet.password(min_length: 8) }
     name { Faker::Name.name }
-    kana { "カナテスト" }
-    phone_number { "000-0000-0000" }
+    sequence(:kana) {|n| "カナテスト#{n}" }
+    sequence(:phone_number) {|n| "000-0000-#{n}" }
     status { 1 }
   end
 end
