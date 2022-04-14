1d2d53be639f0a4bf1c72d3da033564504eb400a
takamuu <mr.takamuu@gmail.com>
2022-04-14 23:19:39 +0900

    Added update_params and edit method to registration_controller.rb #100

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,7 +2,25 @@ module Api
   module V1
     module Auth
       class RegistrationsController < DeviseTokenAuth::RegistrationsController
+        before_action :set_user, only: %i[edit]
+
+        def edit
+          if @user
+            render json: @user, status: :ok
+          else
+            render json: [], status: :no_content
+          end
+        end
+
         private
+
+          def set_user
+            @user = User.find_by(id: account_update_params[:id].to_i)
+          end
+
+          def account_update_params
+            params.require(:user).permit(:id, :name, :kana, :phone_number)
+          end
 
           def sign_up_params
             params.permit(:email, :password, :password_confirmation, :name, :kana, :phone_number)
