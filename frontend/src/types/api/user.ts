b23c5daa1dd2c042dcc7f75d1442be46c14bad01
takamuu <mr.takamuu@gmail.com>
2022-04-15 21:10:57 +0900

    Fixed type User #100

--- frontend/src/types/api/user.ts
+++ frontend/src/types/api/user.ts
@@ -1,11 +1,11 @@
 export type User = {
-  id: number;
-  email: string;
-  name: string;
-  kana: string;
-  nickname: string;
-  phone_number: string;
-  image: string;
-  stripe_id: string;
-  status: string;
+  id?: string;
+  email?: string;
+  name?: string;
+  kana?: string;
+  nickname?: string;
+  phone_number?: string;
+  image?: string;
+  stripe_id?: string;
+  status?: string;
 };
