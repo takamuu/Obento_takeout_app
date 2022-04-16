e6641f911cd41489933f52e4bca4cbf817d62d24
takamuu <mr.takamuu@gmail.com>
2022-04-16 15:50:09 +0900

    Remove edit method to registrations_controller.rb #100

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,21 +2,7 @@ module Api
   module V1
     module Auth
       class RegistrationsController < DeviseTokenAuth::RegistrationsController
-        before_action :set_user, only: %i[edit]
-
-        def edit
-          if @user
-            render json: @user, status: :ok
-          else
-            render json: [], status: :no_content
-          end
-        end
-
         private
-
-          def set_user
-            @user = current_api_v1_user
-          end
 
           def account_update_params
             params.require(:user).permit(:id, :name, :kana, :phone_number)
