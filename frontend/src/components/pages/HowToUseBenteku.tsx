3c4ece0220c82cb9ce2e2764accaa0600ea43ad4
takamuu <mr.takamuu@gmail.com>
2022-04-19 10:29:31 +0900

    Fix layout #154

--- frontend/src/components/pages/HowToUseBenteku.tsx
+++ frontend/src/components/pages/HowToUseBenteku.tsx
@@ -18,7 +18,7 @@ import PickUp from 'images/PickUp.jpg';
 
 export const HowToUseBenteku: VFC = memo(() => {
   return (
-    <VStack h={{ sm: '110vh', md: '60vh' }}>
+    <VStack h={{ sm: '118vh', md: '60vh' }}>
       <Text
         paddingTop={{ sm: '12', md: '20' }}
         paddingBottom={{ sm: '16' }}
@@ -28,12 +28,13 @@ export const HowToUseBenteku: VFC = memo(() => {
       >
         弁テクの使い方
       </Text>
-      <HStack display={{ sm: 'none', md: 'flex' }} spacing={4}>
+      <HStack display={{ sm: 'none', md: 'flex' }}>
         <Image w={{ md: '250px' }} src={SelectADish} />
+        <Spacer p={2} />
         <Image w={{ md: '250px' }} src={PlaceOrder} />
         <Image w={{ md: '250px' }} src={PickUp} />
       </HStack>
-      <VStack display={{ sm: 'flex', md: 'none' }} spacing={4}>
+      <VStack display={{ sm: 'flex', md: 'none' }} spacing={10}>
         <Image w={{ sm: '250px' }} src={SelectADish} />
         <Image w={{ sm: '250px' }} src={PlaceOrder} />
         <Image w={{ sm: '250px' }} src={PickUp} />
