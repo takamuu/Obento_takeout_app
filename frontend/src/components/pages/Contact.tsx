b8b090f7e1351b1fd9ad5b6d15b2f899f8a19ce3
takamuu <mr.takamuu@gmail.com>
2022-04-20 13:59:05 +0900

    Fix Contact page

--- frontend/src/components/pages/Contact.tsx
+++ frontend/src/components/pages/Contact.tsx
@@ -1,59 +1,53 @@
 /* eslint-disable arrow-body-style */
-import { ChangeEvent, memo, useState, VFC } from 'react';
+import { memo, useState, VFC } from 'react';
 import { Button, Input, Textarea } from '@chakra-ui/react';
-import {
-  Box,
-  Divider,
-  Flex,
-  Spacer,
-  Stack,
-  Text,
-  VStack,
-} from '@chakra-ui/layout';
+import { Divider, Spacer, Stack, Text, VStack } from '@chakra-ui/layout';
 
 export const Contact: VFC = memo(() => {
-  const [contactTitle, setContactTitle] = useState('');
+  const [value, setValue] = useState('');
 
-  const onChangeContactTitle = (e: ChangeEvent<HTMLInputElement>) =>
-    setContactTitle(e.target.value);
+  const handleInputChange = (e) => {
+    const inputValue = e.target.value;
+    setValue(inputValue);
+  };
 
   return (
-    <Flex bg="gray.200" align="center" justify="center" height="70vh">
-      <Box bg="white" w={'md'} h={'lg'} p={2} borderRadius="md" shadow="md">
-        <VStack
-          paddingTop="3"
-          fontSize="23px"
-          fontWeight="bold"
-          color="brand"
-          spacing="12px"
-        >
-          <Text>お問い合わせ</Text>
-        </VStack>
-        <Divider borderColor="brand" my={4} />
-        <Stack spacing={4} py={4} px={10}>
-          <Text h="2">件名（必須）</Text>
+    <>
+      <VStack align="center">
+        <Stack spacing={4} w={500}>
+          <Text
+            pt={10}
+            fontSize="24px"
+            fontWeight="bold"
+            color="brand"
+            align={'center'}
+          >
+            お問い合わせ
+          </Text>
+          <Divider p={2} w={'97.5%'} borderColor="brand" />
+          <Text>件名（必須）</Text>
           <Input
             borderColor="gray.300"
             placeholder="件名を入力してください"
             _placeholder={{ color: 'gray.300' }}
-            _hover={{ color: 'gray.600' }}
-            value={contactTitle}
-            onChange={onChangeContactTitle}
           />
-          <Text h="2">お問い合わせ内容（必須）</Text>
+          <Spacer />
+          <Text>お問い合わせ内容（必須）</Text>
           <Textarea
-            h={'28'}
+            h={'36'}
             borderColor="gray.300"
             placeholder="お問い合わせ内容を入力してください"
             _placeholder={{ color: 'gray.300' }}
             _hover={{ color: 'gray.600' }}
+            onChange={handleInputChange}
           />
-          <Spacer />
+          <Spacer p={2} />
           <Button bg="brand" color="white" _hover={{ opacity: 0.8 }}>
             送信する
           </Button>
+          <Spacer p={10} />
         </Stack>
-      </Box>
-    </Flex>
+      </VStack>
+    </>
   );
 });
