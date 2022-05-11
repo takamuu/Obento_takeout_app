bc27aac5b39510efe68c13b67132af6e19a34ae1
takamuu <mr.takamuu@gmail.com>
2022-05-11 15:19:53 +0900

    Remove validates default 0 to contact.rb #147

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ApplicationRecord
   validates :title,     presence: true, length: { maximum: 100 }
   validates :content,   presence: true, length: { maximum: 2000 }
   validates :remote_ip, presence: true
-  validates :status,    presence: true, default: 0
+  validates :status,    presence: true
 
   enum status: {
     not_compatible: 0,
