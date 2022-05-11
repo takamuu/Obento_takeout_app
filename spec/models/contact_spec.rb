e9b670b9b54a90fcc1c4adcac9b44bbabe909a86
takamuu <mr.takamuu@gmail.com>
2022-05-12 07:27:55 +0900

    Added contact model test #147

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,54 @@
 require "rails_helper"
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "バリデーションのチェック" do
+    subject { contact.valid? }
+
+    context "データが条件を満たす時" do
+      let(:contact) { build(:contact) }
+      it "保存できる" do
+        expect(subject).to eq true
+      end
+    end
+
+    context "titleが空欄の場合" do
+      let(:contact) { build(:contact, title: "") }
+      it "保存できない" do
+        expect(subject).to eq false
+      end
+    end
+
+    context "titleが100文字を超える場合" do
+      let(:contact) { build(:contact, title: "a" * 101) }
+      it "保存できない" do
+        expect(subject).to eq false
+      end
+    end
+
+    context "contentが空欄の場合" do
+      let(:contact) { build(:contact, content: "") }
+      it "保存できない" do
+        expect(subject).to eq false
+      end
+    end
+
+    context "contentが2000文字を超える場合" do
+      let(:contact) { build(:contact, content: "a" * 2001) }
+      it "保存できない" do
+        expect(subject).to eq false
+      end
+    end
+
+    context "ユーザーが削除されたとき" do
+      subject { @contact.user.destroy }
+
+      before {
+        @contact = create(:contact)
+      }
+
+      it "お問い合わせも削除される" do
+        expect { subject }.to change { Contact.count }.by(-1)
+      end
+    end
+  end
 end
