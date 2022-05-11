c29289b4da13607cbbbeecb04ca96be7d5748978
takamuu <mr.takamuu@gmail.com>
2022-05-11 20:58:22 +0900

    Fix contact route #147

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,8 @@ Rails.application.routes.draw do
       resources :orders, only: %i[index create]
 
       mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
+      resources :contacts, only: %i[create]
     end
   end
 
