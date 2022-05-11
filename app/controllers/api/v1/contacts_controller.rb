348dfe4ea4a4f5e0033e6dd8f2b286a514751f5e
takamuu <mr.takamuu@gmail.com>
2022-05-11 20:59:30 +0900

    Add Api V1 #147

--- app/controllers/api/v1/contacts_controller.rb
+++ app/controllers/api/v1/contacts_controller.rb
@@ -1,23 +1,29 @@
-class ContactsController < ApplicationController
-  def create
-    @contact = Contact.new(contact_params)
-    if @contact.save
-      # ユーザーにメールを送信
-      ContactMailer.user_email(@contact).deliver_now
-      # 管理者にメールを送信
-      ContactMailer.admin_email(@contact).deliver_now
-      render status: :ok
-    else
-      render status: :internal_server_error
+module Api
+  module V1
+    class ContactsController < ApplicationController
+      before_action :authenticate_api_v1_user!
+
+      def create
+        @contact = Contact.new(contact_params)
+        if @contact.save
+          # ユーザーにメールを送信
+          ContactMailer.user_email(@contact).deliver_now
+          # 管理者にメールを送信
+          ContactMailer.admin_email(@contact).deliver_now
+          render status: :ok
+        else
+          render status: :internal_server_error
+        end
+      end
+
+      private
+
+        # IPアドレスをパラメータに追加
+        def contact_params
+          params.require(:contact).
+            permit(:title, :content).
+            merge(user_id: current_api_v1_user.id, remote_ip: request.remote_ip)
+        end
     end
   end
-
-  private
-
-    # IPアドレスをパラメータに追加
-    def contact_params
-      params.require(:contact).
-        permit(:user, :title, :content, :status).
-        merge(remote_ip: request.remote_ip)
-    end
 end
