cb74414f063fd88bc5529884e908c2465c6436ea
takamuu <mr.takamuu@gmail.com>
2022-05-17 14:06:19 +0900

    Added eslint-disable #159

--- frontend/src/components/pages/Foods.tsx
+++ frontend/src/components/pages/Foods.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-constant-condition */
 /* eslint-disable arrow-body-style */
