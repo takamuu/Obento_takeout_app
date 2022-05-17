a88a1f3b25f79b29df292583be3e8836d898cc8d
takamuu <mr.takamuu@gmail.com>
2022-05-17 14:04:36 +0900

    Fix order_serializer.rb #159

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,8 +1,12 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :rceipt_number, :total_price, :consumption_tax, :progress_status, :restaurant_name, :created_at
+  attributes :id, :user_id, :rceipt_number, :total_price, :consumption_tax, :progress_status, :restaurant_name, :restaurant, :created_at
   has_many :order_details
 
   def restaurant_name
     object.order_details.first.food.restaurant.name
+  end
+
+  def restaurant
+    object.order_details.first.food.restaurant
   end
 end
