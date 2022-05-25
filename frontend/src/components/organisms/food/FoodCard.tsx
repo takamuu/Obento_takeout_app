349a4357373530448d8ac136de7bbb11fa7481c0
takamuu <mr.takamuu@gmail.com>
2022-05-25 22:09:40 +0900

    Fix for smartphone support #164

--- frontend/src/components/organisms/food/FoodCard.tsx
+++ frontend/src/components/organisms/food/FoodCard.tsx
@@ -16,8 +16,8 @@ export const FoodCard: VFC<Props> = memo((props) => {
   const { id, imageUrl, foodName, foodDescription, foodPrice, onClick } = props;
   return (
     <Box
-      w="400px"
-      h="180px"
+      w={{ base: '360px', md: '400px' }}
+      h={{ base: '162px', md: '180px' }}
       borderWidth="1px"
       shadow="md"
       overflow="hidden"
@@ -25,18 +25,30 @@ export const FoodCard: VFC<Props> = memo((props) => {
       onClick={() => onClick(id)}
     >
       <HStack>
-        <Image w="240px" h="180px" src={imageUrl} alt={foodName} m="auto" />
+        <Image
+          w={{ base: '216px', md: '240px' }}
+          h={{ base: '162px', md: '180px' }}
+          src={imageUrl}
+          alt={foodName}
+          m="auto"
+        />
         <VStack>
           <Text
-            w="150px"
-            fontSize="lg"
+            w={{ base: '135px', md: '150px' }}
+            fontSize={{ base: 'md', md: 'lg' }}
             fontWeight="bold"
             color="brand"
             textAlign="center"
           >
             {foodName}
           </Text>
-          <Text fontWeight={'normal'} w="140px" color="brand" isTruncated>
+          <Text
+            w={{ base: '126px', md: '140px' }}
+            fontSize={{ base: 'sm', md: 'lg' }}
+            fontWeight={'normal'}
+            color="brand"
+            isTruncated
+          >
             {foodDescription}
           </Text>
           <Text fontWeight={'bold'} color="brand">
