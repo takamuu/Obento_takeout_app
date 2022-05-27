9ae33e94bbcdc5c188a009de599d811aef16fee4
takamuu <mr.takamuu@gmail.com>
2022-05-27 11:36:50 +0900

    Add restaurant and confirm to Cart.tsx #142

--- frontend/src/components/pages/Cart.tsx
+++ frontend/src/components/pages/Cart.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable arrow-body-style */
-import { memo, useEffect, useState, VFC } from 'react';
-import { Box, Center, Text, VStack, Wrap, WrapItem } from '@chakra-ui/layout';
+import { memo, useCallback, useEffect, useState, VFC } from 'react';
+import {
+  Center,
+  Divider,
+  Text,
+  VStack,
+  Wrap,
+  WrapItem,
+} from '@chakra-ui/layout';
 import { Spinner } from '@chakra-ui/spinner';
 
 import { CartButton } from 'components/atoms/button/CartButton';
@@ -14,6 +21,9 @@ import { usePostOrders } from 'hooks/usePostOrders';
 import { useLoginUser } from 'hooks/useLoginUser';
 import { useDisclosure } from '@chakra-ui/react';
 import { ReceiptModal } from 'components/organisms/order/ReceiptModal';
+import { useRestaurants } from 'hooks/useRestaurants';
+import { RestaurantCard } from 'components/organisms/restaurant/RestaurantCard';
+import { useHistory } from 'react-router-dom';
 
 export const Cart: VFC = memo(() => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -23,6 +33,7 @@ export const Cart: VFC = memo(() => {
   const { deleteCartDetails } = useDeleteCartDetails();
   const { postOrders, order, loading: orderLoading } = usePostOrders();
   const [newCarts, setNewCarts] = useState<NewCarts>([]);
+  const { getRestaurants, restaurants } = useRestaurants();
 
   const totalAmount = newCarts.reduce(
     (total, newCart) => total + newCart.amount,
@@ -37,9 +48,11 @@ export const Cart: VFC = memo(() => {
   };
 
   const onOrderButton = async (userId: string) => {
-    await postOrders(userId).then(() => {
-      onOpen();
-    });
+    const confirmOrder = confirm('注文を確定してよろしいですか？');
+    if (confirmOrder)
+      await postOrders(userId).then(() => {
+        onOpen();
+      });
   };
 
   const onClickDelete = (foodId: string) => {
@@ -47,7 +60,18 @@ export const Cart: VFC = memo(() => {
     setNewCarts((s) => s.filter((cart) => String(cart.food.id) !== foodId));
   };
 
+  const history = useHistory();
+  const onClickRestaurant = useCallback(
+    (restaurant) =>
+      history.push({
+        pathname: `restaurants/${restaurant.id}/foods`,
+        state: { restaurant },
+      }),
+    []
+  );
+
   useEffect(() => {
+    getRestaurants();
     if (carts.length)
       setNewCarts(
         carts.map((cart) => ({
@@ -70,77 +94,77 @@ export const Cart: VFC = memo(() => {
           <Spinner />
         </Center>
       ) : (
-        <Wrap justify={'center'}>
-          <Box fontSize={'4xl'} p={4} w={'full'} textAlign={'center'}>
-            カートページ
-          </Box>
-          <Wrap p={{ base: 4, md: 10 }}>
-            {newCarts.length ? (
-              <>
-                <VStack>
-                  {newCarts.map((cart, i) => (
-                    <WrapItem key={i}>
-                      <CartCard
-                        foodId={String(cart.food.id)}
-                        foodName={cart.food.name}
-                        count={cart.count}
-                        price={cart.price}
-                        onDelete={() => {
-                          onClickDelete(String(cart.food.id));
-                        }}
-                        onChangeCount={(newCount) => {
-                          setNewCarts((s) =>
-                            s.map((newCart) => {
-                              if (newCart.id !== cart.food.id) return newCart;
-                              return {
-                                ...newCart,
-                                amount: cart.food.price * newCount,
-                                count: newCount,
-                              };
-                            })
-                          );
-                          {
-                            updateCart({
-                              food_id: cart.food.id,
+        <Wrap paddingTop={10} paddingBottom={20} justify={'center'}>
+          {newCarts.length ? (
+            <>
+              <VStack>
+                {restaurants.map((restaurant, i) => {
+                  if (restaurant.id === newCarts[0].food.restaurant_id)
+                    return (
+                      <WrapItem paddingBottom={10} key={i}>
+                        <RestaurantCard
+                          imageUrl={`${process.env.PUBLIC_URL}${restaurant.image}`}
+                          restaurantName={restaurant.name}
+                          restaurantDescription={restaurant.description}
+                          onClick={() => onClickRestaurant(restaurant)}
+                        />
+                      </WrapItem>
+                    );
+                })}
+                {newCarts.map((cart, i) => (
+                  <WrapItem key={i}>
+                    <CartCard
+                      foodId={String(cart.food.id)}
+                      foodName={cart.food.name}
+                      count={cart.count}
+                      price={cart.price}
+                      onDelete={() => {
+                        onClickDelete(String(cart.food.id));
+                      }}
+                      onChangeCount={(newCount) => {
+                        setNewCarts((s) =>
+                          s.map((newCart) => {
+                            if (newCart.id !== cart.food.id) return newCart;
+                            return {
+                              ...newCart,
+                              amount: cart.food.price * newCount,
                               count: newCount,
-                            });
-                          }
-                        }}
-                      />
-                    </WrapItem>
-                  ))}
-                </VStack>
-              </>
-            ) : (
-              <Text fontSize={'xl'} fontWeight={'bold'}>
-                カートの中に商品はありません
-              </Text>
-            )}
-          </Wrap>
-          <VStack w={'full'} h="30vh">
-            <Box
-              color={'gray.300'}
-              borderTop={'2px'}
-              bg="twitter.100"
-              w={'400px'}
-              verticalAlign={'center'}
-            ></Box>
-            <Text
-              p={4}
-              color={'brand'}
-              fontSize={'2xl'}
-              fontWeight={'bold'}
-              verticalAlign={'center'}
-            >
-              合計 {`¥${totalAmount.toLocaleString()}`}
+                            };
+                          })
+                        );
+                        {
+                          updateCart({
+                            food_id: cart.food.id,
+                            count: newCount,
+                          });
+                        }
+                      }}
+                    />
+                  </WrapItem>
+                ))}
+                <Divider w={400} borderWidth={'initial'} borderColor="brand" />
+                <Text
+                  p={4}
+                  color={'brand'}
+                  fontSize={'2xl'}
+                  fontWeight={'bold'}
+                  verticalAlign={'center'}
+                >
+                  合計 {`¥${totalAmount.toLocaleString()}`}
+                </Text>
+                <CartButton
+                  display={display(newCarts)}
+                  onClick={() => onOrderButton(String(loginUser.id))}
+                >
+                  <Text p={2}>注文内容を最終確認して、注文する</Text>
+                </CartButton>
+              </VStack>
+            </>
+          ) : (
+            <Text padding={'20'} h={'50vh'} fontSize={'xl'} fontWeight={'bold'}>
+              カートの中に商品はありません
             </Text>
-            <CartButton
-              display={display(newCarts)}
-              onClick={() => onOrderButton(String(loginUser.id))}
-            >
-              <Text p={2}>注文する</Text>
-            </CartButton>
-          </VStack>
+          )}
         </Wrap>
       )}
       {isOpen && (
